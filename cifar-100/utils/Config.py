6f89c4db2d96ae48c55c4ef067effebca59880bd
yiwei <e-well@outlook.com>
2023-03-25 20:21:45 -0400

    move model_cnt from acq to model config

--- cifar-100/utils/Config.py
+++ cifar-100/utils/Config.py
@@ -2,6 +2,59 @@ import os
 from utils import config
 from abc import abstractmethod
 
+class AcquistionConfig():
+    method: str
+    new_data_number_per_class:int
+
+    def __init__(self) -> None:
+        self.method = ''
+        self.new_data_number_per_class = 0
+
+    def set_items(self, method, new_data_number):
+        self.method = method
+        self.new_data_number_per_class = new_data_number
+
+    @abstractmethod
+    def get_new_data_size(self):
+        pass
+
+    @abstractmethod
+    def get_info(self):
+        pass
+
+class NonSeqAcquistionConfig(AcquistionConfig):
+    def __init__(self) -> None:
+        super().__init__()
+
+    def get_new_data_size(self, class_number):
+        return class_number*self.new_data_number_per_class
+    
+    def get_info(self):
+        return 'acquisition method: {}, n_data_per_class:{}'.format(self.method, self.new_data_number_per_class)
+
+class SequentialAcConfig(AcquistionConfig):
+    def __init__(self, sequential_rounds:int) -> None:
+        super().__init__()
+        self.sequential_rounds = sequential_rounds
+        self.round_acquire_method = 'dv'
+        self.current_round = 0
+    def set_round(self, round):
+        self.current_round = round + 1
+    def get_new_data_size(self, class_number):
+        return class_number*self.round_data_per_class*self.sequential_rounds
+    def get_info(self):
+        return 'acquisition method: {}, n_data_per_class:{} in round {}'.format(self.method, self.round_data_per_class, self.current_round)
+
+    def set_items(self, method, new_data_number):
+        super().set_items(method, new_data_number)
+        self.round_data_per_class = self.new_data_number_per_class  // self.sequential_rounds
+
+def AcquistionConfigFactory(method, sequential_rounds):
+    if method == 'non_seq':
+        return NonSeqAcquistionConfig()
+    else:
+        return SequentialAcConfig(sequential_rounds)
+
 class ModelConfig():
     batch_size: int
     class_number: int
@@ -21,79 +74,27 @@ class OldModelConfig(ModelConfig):
         super().__init__(batch_size, class_number, model_dir)
         self.path = os.path.join(self.root,'{}.pt'.format(model_cnt))
 class NewModelConfig(ModelConfig):
-    def __init__(self, batch_size, class_number, model_dir, pure, setter) -> None:
+    def __init__(self, batch_size, class_number, model_dir, model_cnt, pure, setter) -> None:
         super().__init__(batch_size, class_number, model_dir)
         self.pure = pure
         self.setter = setter
         self.path = ''
+        self.model_cnt = model_cnt
         self.set_root(setter)
-    def set_path(self,acquistion_config):
-        acq_root = os.path.join(self.root,acquistion_config.method)
-        self.check_dir(acq_root)
-        self.path = os.path.join(acq_root, '{}_{}.pt'.format(acquistion_config.new_data_number_per_class,acquistion_config.model_cnt))
+    def set_path(self,acquistion_config:AcquistionConfig):
+        self.path = os.path.join(self.root, '{}_{}.pt'.format(acquistion_config.method, acquistion_config.new_data_number_per_class))
     def set_root(self,setter):
         pure_name = 'pure' if self.pure else 'non-pure'
-        self.root = os.path.join(self.root,setter,pure_name) 
+        self.root = os.path.join(self.root,setter,pure_name, str(self.model_cnt)) 
         self.check_dir(self.root)
 
 class LogConfig(NewModelConfig):
-    def __init__(self, batch_size, class_number, model_dir, pure, setter) -> None:
-        super().__init__(batch_size, class_number, model_dir, pure, setter)
+    def __init__(self, batch_size, class_number, model_dir, model_cnt, pure, setter) -> None:
+        super().__init__(batch_size, class_number, model_dir, model_cnt, pure, setter)
+        self.set_log_root()
+    def set_log_root(self):
         self.root = os.path.join(self.root,'log')
-
-class AcquistionConfig():
-    method: str
-    new_data_number_per_class:int
-    model_cnt: int
-
-    def __init__(self, model_cnt) -> None:
-        self.method = ''
-        self.new_data_number_per_class = 0
-        self.model_cnt = model_cnt
-
-    def set_items(self, method, new_data_number):
-        self.method = method
-        self.new_data_number_per_class = new_data_number
-
-    @abstractmethod
-    def get_new_data_size(self):
-        pass
-
-    @abstractmethod
-    def get_info(self):
-        pass
-
-class NonSeqAcquistionConfig(AcquistionConfig):
-    def __init__(self, model_cnt) -> None:
-        super().__init__(model_cnt)
-
-    def get_new_data_size(self, class_number):
-        return class_number*self.new_data_number_per_class
-    
-    def get_info(self):
-        return self.method + ' ' + str(self.new_data_number_per_class) + ' ' + str(self.model_cnt)
-
-class SequentialAcConfig(AcquistionConfig):
-    def __init__(self, model_cnt, sequential_rounds:int) -> None:
-        super().__init__(model_cnt)
-        self.sequential_rounds = sequential_rounds
-        self.round_acquire_method = 'dv'
-        self.current_round = 0
-    def set_round(self, round):
-        self.current_round = round + 1
-    def get_new_data_size(self, class_number):
-        return class_number*self.round_data_per_class*self.sequential_rounds
-    def get_info(self):
-        return self.method + ' ' + str(self.new_data_number_per_class) + str(self.round_data_per_class) + ' ' + str(self.model_cnt) + ' ' + str(self.current_round) 
-    def set_items(self, method, new_data_number):
-        super().set_items(method, new_data_number)
-        self.round_data_per_class = self.new_data_number_per_class  // self.sequential_rounds
-
-def AcquistionConfigFactory(method, model_cnt, sequential_rounds):
-    if method == 'non_seq':
-        return NonSeqAcquistionConfig(model_cnt)
-    else:
-        return SequentialAcConfig(model_cnt, sequential_rounds)
+        self.check_dir(self.root)
 
 def parse_config(model_dir, pure:bool):
     pure_name = 'pure' if pure else 'non-pure'
