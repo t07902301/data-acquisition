2abd769e3089b5ea188f5058d0cd4744a9acbec3
yiwei <e-well@outlook.com>
2023-03-26 22:52:35 -0400

    use strategy class

--- cifar-100/main.py
+++ cifar-100/main.py
@@ -1,57 +1,41 @@
 from utils.strategy import *
 
-def run(ds:DataSplits, old_model_config, new_model_config, acquisition_config:AcquistionConfig, methods, new_img_num_list):
+def run(ds:DataSplits, methods_list, new_img_num_list, old_model_config:OldModelConfig, new_model_config:NewModelConfig, acquire_instruction:AcquistionConfig):
     subset_length_record = {
         k: len(ds.dataset[k]) for k in ds.dataset.keys() 
     }
 
     percent_epoch = []
-
-    for method in methods:
-        print('In method', method)
+    for method in methods_list:
+        strategy = StrategyFactory(old_model_config, new_model_config, method)
         for new_img_num in new_img_num_list:
-            acquisition_config.set_items(method,new_img_num)
-            if method not in ['seq','seq_clf']:
-                strategy = NonSeqStrategy(ds,old_model_config, new_model_config)
-                new_model = strategy.get_new_model(acquisition_config)
-                # new_model = non_seq(ds, old_model_config, new_model_config, acquisition_config)
-            else:
-                strategy = SeqStrategy(ds,old_model_config, new_model_config)
-                new_model, minority_cnt = strategy.get_new_model(acquisition_config)
-                # new_model, minority_cnt = seq(ds, old_model_config, new_model_config, seq_acq_config)
-                percent_epoch.append(minority_cnt/new_img_num)
-
-            new_model_config.set_path(acquisition_config)
-            save_model(new_model,new_model_config.path)
-
-            del new_model
-
+            acquire_instruction.set_items(method,new_img_num)
+            strategy.operate(acquire_instruction, ds)
             for split in ds.dataset.keys():
                 assert subset_length_record[split] == len(ds.dataset[split]), 'init {} is changed'.format(split)
 
     return percent_epoch
 
-def main(epochs, new_model_setter='retrain', pure=False, model_dir ='', methods='', seq_rounds=1):
+def main(epochs, new_model_setter='retrain', pure=False, model_dir ='', strategy='', seq_rounds=1):
     print('Use pure: ',pure)
     percent_list = []
     # method_list =['conf']
     # new_img_num_list = [50]
     batch_size, select_fine_labels, label_map, new_img_num_list, superclass_num = parse_config(model_dir, pure)
-    method_list = ['dv','sm','conf','mix'] if methods=='non_seq' else [methods]
+    method_list = ['dv','sm','conf','mix'] if strategy=='non_seq' else [strategy]
+    # method_list = ['sm','mix']
+    # method_list, new_img_num_list = ['dv','sm','conf'], [25,50,75]
+    # new_img_num_list,method_list = [150], ['dv']
+
+    ds_list = get_data_splits_list(epochs, select_fine_labels, model_dir, label_map)
 
     for epo in range(epochs):
-
         print('in epoch {}'.format(epo))
-
-        ds = DataSplits(data_config['ds_root'],select_fine_labels,model_dir)
-        if select_fine_labels!=[]:
-            ds.modify_coarse_label(label_map)
-
+        ds = ds_list[epo]
         old_model_config = OldModelConfig(batch_size,superclass_num,model_dir, epo)
-        new_model_config = NewModelConfig(batch_size,superclass_num,model_dir,pure, new_model_setter)
-        acquistion_config = AcquistionConfigFactory(methods,epo, seq_rounds)
-
-        percent_epoch = run(ds,old_model_config,new_model_config, acquistion_config, method_list, new_img_num_list)
+        new_model_config = NewModelConfig(batch_size,superclass_num,model_dir, epo, pure, new_model_setter)
+        acquire_instruction = AcquistionConfigFactory(strategy,seq_rounds)
+        percent_epoch = run(ds,method_list, new_img_num_list, old_model_config,new_model_config, acquire_instruction)
         percent_list.append(percent_epoch)
 
     percent_list = np.array(percent_list)
@@ -66,9 +50,9 @@ if __name__ == '__main__':
     parser.add_argument('-p','--pure',type=bool,default=False)
     parser.add_argument('-d','--model_dir',type=str,default='')
     parser.add_argument('-r','--rounds',type=int,default=2)
-    parser.add_argument('-m','--methods',type=str)
+    parser.add_argument('-s','--strategy',type=str)
 
     args = parser.parse_args()
     # method, new_img_num, save_model
-    main(args.epochs,pure=args.pure,model_dir=args.model_dir,seq_rounds=args.rounds,methods=args.methods)
+    main(args.epochs,pure=args.pure,model_dir=args.model_dir,seq_rounds=args.rounds,strategy=args.strategy)
     # print(args.method)
