2e617b37cd060cf7e0ba4d1bf51f40ba29ce0e42
yiwei <e-well@outlook.com>
2023-03-26 22:56:58 -0400

    log indices only for non-seq

--- cifar-100/utils/log.py
+++ cifar-100/utils/log.py
@@ -6,13 +6,12 @@ def log_data(data, model_cofig:NewModelConfig, acquisition_config):
     torch.save(data, log_config.path)
 
 def log_indices(indices, model_cofig:NewModelConfig, acquisition_config:AcquistionConfig):
-    if model_cofig.pure:
-        log_config = LogConfig(batch_size=model_cofig.batch_size,class_number=model_cofig.class_number,model_dir=model_cofig.model_dir, model_cnt=model_cofig.model_cnt,pure=model_cofig.pure,setter=model_cofig.setter)
-        log_config.root = os.path.join(log_config.root, 'indices')
-        log_config.check_dir(log_config.root)
-        log_config.set_path(acquisition_config)
-        torch.save(indices, log_config.path)
-        print('indices log save to', log_config.path)
+    log_config = LogConfig(batch_size=model_cofig.batch_size,class_number=model_cofig.class_number,model_dir=model_cofig.model_dir, model_cnt=model_cofig.model_cnt,pure=model_cofig.pure,setter=model_cofig.setter)
+    log_config.root = os.path.join(log_config.root, 'indices')
+    log_config.check_dir(log_config.root)
+    log_config.set_path(acquisition_config)
+    torch.save(indices, log_config.path)
+    print('indices log save to', log_config.path)
 
 # def log_max_dv(model_cofig:NewModelConfig, acquisition_config:AcquistionConfig, dv_list):
 #     log_config = LogConfig(batch_size=model_cofig.batch_size,class_number=model_cofig.class_number,model_dir=model_cofig.model_dir, model_cnt=model_cofig.model_cnt,pure=model_cofig.pure,setter=model_cofig.setter)
