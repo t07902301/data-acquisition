ed8f52710fcaff810287fbc1ec930f009755a8e9
yiwei <e-well@outlook.com>
2023-03-30 21:36:23 -0400

    extract model config from strategy fields

--- cifar-100/utils/strategy.py
+++ cifar-100/utils/strategy.py
@@ -6,42 +6,45 @@ from copy import deepcopy
 from utils.log import *
 from abc import abstractmethod
 class Strategy():
-    old_model_config: OldModelConfig
-    new_model_config: NewModelConfig
-    def __init__(self, old_model_config, new_model_config) -> None:
-        self.old_model_config = old_model_config
-        self.new_model_config = new_model_config
+    def __init__(self) -> None:
+        pass
     @abstractmethod
-    def operate(self, acquire_instruction: AcquistionConfig, data_splits: DataSplits):
+    def operate(self, acquire_instruction: AcquistionConfig, data_splits: DataSplits, old_model_config:OldModelConfig, new_model_config:NewModelConfig):
+        '''
+        Under some acquistion instruction (which method, how much new data), this strategy operates on data and the old model. Finally, the new model will be saved.
+        '''
         pass
 
 class NonSeqStrategy(Strategy):
-    def __init__(self, old_model_config, new_model_config) -> None:
-        super().__init__(old_model_config, new_model_config)
+    def __init__(self) -> None:
+        super().__init__()
     @abstractmethod
-    def get_new_data_indices(self, n_data, data_splits: DataSplits):
+    def get_new_data_indices(self, n_data, data_splits: DataSplits, old_model_config:OldModelConfig):
+        '''
+        Based on old model performance, a certain number of new data is acquired.
+        '''
         pass
-    def operate(self, acquire_instruction: AcquistionConfig, data_splits: DataSplits):
+    def operate(self, acquire_instruction: AcquistionConfig, data_splits: DataSplits, old_model_config:OldModelConfig, new_model_config:NewModelConfig):
         ds = deepcopy(data_splits)
-        ds.get_dataloader(self.new_model_config.batch_size)
-        new_data_indices, raw_new_data_indices = self.get_new_data_indices(acquire_instruction.new_data_number_per_class, ds)
-        new_data_set = torch.utils.data.Subset(ds.dataset['market_aug'],new_data_indices)
-        ds.use_new_data(new_data_set, self.new_model_config, acquire_instruction)
-        new_model = get_new_model(self.new_model_config, ds.loader['train'], ds.loader['val'])
-        self.new_model_config.set_path(acquire_instruction)
-        save_model(new_model,self.new_model_config.path)
-        # log_data(ds.dataset['train'], self.new_model_config, acquire_instruction)
-        log_indices(raw_new_data_indices, self.new_model_config, acquire_instruction)
+        ds.get_dataloader(new_model_config.batch_size)
+        new_data_indices, raw_new_data_indices = self.get_new_data_indices(acquire_instruction.new_data_number_per_class, ds, old_model_config)
+        new_data_set = torch.utils.data.Subset(ds.dataset['market'],new_data_indices)
+        ds.use_new_data(new_data_set, new_model_config, acquire_instruction)
+        new_model = get_new_model(new_model_config, ds.loader['train'], ds.loader['val'])
+        new_model_config.set_path(acquire_instruction)
+        save_model(new_model,new_model_config.path)
+        # log_data(ds.dataset['train'], new_model_config, acquire_instruction)
+        log_indices(raw_new_data_indices, new_model_config, acquire_instruction)
         
 class Greedy(NonSeqStrategy):
-    def __init__(self, old_model_config, new_model_config) -> None:
-        super().__init__(old_model_config, new_model_config)
-    def get_new_data_indices(self, n_data, data_splits: DataSplits):
-        base_model = load_model(self.old_model_config)
+    def __init__(self) -> None:
+        super().__init__()
+    def get_new_data_indices(self, n_data, data_splits: DataSplits, old_model_config: OldModelConfig):
+        base_model = load_model(old_model_config)
         clf,clip_processor,_ = get_CLF(base_model,data_splits.loader)
         market_info = apply_CLF(clf,data_splits.loader['market'],clip_processor)
         new_data_indices_total = []
-        for c in range(self.new_model_config.class_number):
+        for c in range(old_model_config.class_number):
             cls_indices, cls_mask, cls_dv = get_class_info(c, market_info['gt'], market_info['dv'])
             sorted_idx = np.argsort(cls_dv) # index of images ordered by their decision values
             new_data_indices = get_top_values(cls_indices[sorted_idx],n_data)
@@ -50,25 +53,25 @@ class Greedy(NonSeqStrategy):
         return np.concatenate(new_data_indices_total), new_data_indices_total       
 
 class Sample(NonSeqStrategy):
-    def __init__(self, old_model_config, new_model_config) -> None:
-        super().__init__(old_model_config, new_model_config)
-    def get_new_data_indices(self, n_data, data_splits: DataSplits):
+    def __init__(self) -> None:
+        super().__init__()
+    def get_new_data_indices(self, n_data, data_splits: DataSplits, old_model_config: OldModelConfig):
         market_gts = get_loader_labels(data_splits.loader['market'])
         new_data_indices_total = []
-        for c in range(self.new_model_config.class_number):
+        for c in range(old_model_config.class_number):
             cls_indices, cls_mask, _ = get_class_info(c, market_gts)  
             new_data_indices = sample_acquire(cls_indices,n_data)
             new_data_indices_total.append(new_data_indices)
         return np.concatenate(new_data_indices_total), new_data_indices_total       
 
 class Confidence(NonSeqStrategy):
-    def __init__(self, old_model_config, new_model_config) -> None:
-        super().__init__(old_model_config, new_model_config)
-    def get_new_data_indices(self, n_data, data_splits: DataSplits):
-        base_model = load_model(self.old_model_config)
+    def __init__(self) -> None:
+        super().__init__()
+    def get_new_data_indices(self, n_data, data_splits: DataSplits, old_model_config: OldModelConfig):
+        base_model = load_model(old_model_config)
         market_gts, market_preds, market_confs = evaluate_model(data_splits.loader['market'], base_model)
         new_data_indices_total = []
-        for c in range(self.new_model_config.class_number):   
+        for c in range(old_model_config.class_number):   
             cls_indices, cls_mask, _ = get_class_info(c, market_gts)  
             cls_conf = market_confs[cls_mask]
             conf_sorted_idx = np.argsort(cls_conf) 
@@ -77,14 +80,14 @@ class Confidence(NonSeqStrategy):
         return np.concatenate(new_data_indices_total), new_data_indices_total       
 
 class Mix(NonSeqStrategy):
-    def __init__(self, old_model_config, new_model_config) -> None:
-        super().__init__(old_model_config, new_model_config)
-    def get_new_data_indices(self, n_data, data_splits: DataSplits):
-        base_model = load_model(self.old_model_config)
+    def __init__(self) -> None:
+        super().__init__()
+    def get_new_data_indices(self, n_data, data_splits: DataSplits, old_model_config: OldModelConfig):
+        base_model = load_model(old_model_config)
         clf,clip_processor,_ = get_CLF(base_model,data_splits.loader)
         market_info = apply_CLF(clf,data_splits.loader['market'],clip_processor)
         new_data_indices_total = []
-        for c in range(self.new_model_config.class_number):
+        for c in range(old_model_config.class_number):
             cls_indices, cls_mask, cls_dv = get_class_info(c, market_info['gt'], market_info['dv'])
             sorted_idx = np.argsort(cls_dv) # index of images ordered by their decision values
             greedy_results = get_top_values(cls_indices[sorted_idx],n_data-n_data//2)
@@ -94,12 +97,12 @@ class Mix(NonSeqStrategy):
         return np.concatenate(new_data_indices_total), new_data_indices_total       
 
 class SeqCLF(Strategy):
-    def __init__(self, old_model_config, new_model_config) -> None:
-        super().__init__(old_model_config, new_model_config)
-    def operate(self, acquire_instruction:SequentialAcConfig, data_splits: DataSplits):
-        self.sub_strategy = StrategyFactory(self.old_model_config, self.new_model_config, acquire_instruction.round_acquire_method)
+    def __init__(self) -> None:
+        super().__init__()
+    def operate(self, acquire_instruction:SequentialAcConfig, data_splits: DataSplits, old_model_config: OldModelConfig, new_model_config:NewModelConfig):
+        self.sub_strategy = StrategyFactory(old_model_config, new_model_config, acquire_instruction.round_acquire_method)
         ds = deepcopy(data_splits)
-        ds.get_dataloader(self.new_model_config.batch_size)
+        ds.get_dataloader(new_model_config.batch_size)
         org_val_ds = ds.dataset['val']
         minority_cnt = 0
         new_data_total_set = None
@@ -110,77 +113,78 @@ class SeqCLF(Strategy):
 
             acquire_instruction.set_round(round_i)
 
-            assert len(ds.dataset['market'])==len(ds.dataset['market_aug']), "market set size not equal to aug market in round {}".format(round_i)
+            # assert len(ds.dataset['market'])==len(ds.dataset['market_aug']), "market set size not equal to aug market in round {}".format(round_i)
 
-            new_data_round_indices,_ = self.sub_strategy.get_new_data_indices(acquire_instruction.round_data_per_class, ds)
+            new_data_round_indices,_ = self.sub_strategy.get_new_data_indices(acquire_instruction.round_data_per_class, ds, old_model_config)
             new_data_round_set = torch.utils.data.Subset(ds.dataset['market_aug'],new_data_round_indices)
             new_data_round_set_no_aug = torch.utils.data.Subset(ds.dataset['market'],new_data_round_indices)
 
-            ds.reduce('market', new_data_round_indices, self.new_model_config.batch_size, acquire_instruction)
-            ds.reduce('market_aug', new_data_round_indices, self.new_model_config.batch_size, acquire_instruction)
-            ds.expand('val', new_data_round_set_no_aug, self.new_model_config.batch_size, acquire_instruction)
+            ds.reduce('market', new_data_round_indices, new_model_config.batch_size, acquire_instruction)
+            ds.reduce('market_aug', new_data_round_indices, new_model_config.batch_size, acquire_instruction)
+            ds.expand('val', new_data_round_set_no_aug, new_model_config.batch_size, acquire_instruction)
 
-            new_data_total_set = new_data_round_set  if (new_data_total_set == None) else  torch.utils.data.ConcatDataset([new_data_total_set,new_data_round_set])
+            # new_data_total_set = new_data_round_set  if (new_data_total_set == None) else  torch.utils.data.ConcatDataset([new_data_total_set,new_data_round_set])
+            new_data_total_set = new_data_round_set_no_aug  if (new_data_total_set == None) else  torch.utils.data.ConcatDataset([new_data_total_set,new_data_round_set_no_aug])
 
-        ds.use_new_data(new_data_total_set, self.new_model_config, acquire_instruction)
+        ds.use_new_data(new_data_total_set, new_model_config, acquire_instruction)
 
-        log_data(ds.dataset['train'], self.new_model_config, acquire_instruction)
+        log_data(ds.dataset['train'], new_model_config, acquire_instruction)
     
-        assert len(org_val_ds) == len(ds.dataset['val']) - acquire_instruction.get_new_data_size(self.new_model_config.class_number), "size error with original val"
+        assert len(org_val_ds) == len(ds.dataset['val']) - acquire_instruction.get_new_data_size(new_model_config.class_number), "size error with original val"
 
-        ds.update_dataset('val', org_val_ds, self.new_model_config.batch_size)
+        ds.update_dataset('val', org_val_ds, new_model_config.batch_size)
 
-        new_model = get_new_model(self.new_model_config, ds.loader['train'], ds.loader['val'])
-        self.new_model_config.set_path(acquire_instruction)
-        save_model(new_model,self.new_model_config.path)
+        new_model = get_new_model(new_model_config, ds.loader['train'], ds.loader['val'])
+        new_model_config.set_path(acquire_instruction)
+        save_model(new_model,new_model_config.path)
 
 class Seq(Strategy):
-    def __init__(self, old_model_config, new_model_config) -> None:
-        super().__init__(old_model_config, new_model_config)   
-    def operate(self, acquire_instruction:SequentialAcConfig, data_splits: DataSplits):
-        self.sub_strategy = StrategyFactory(self.old_model_config, self.new_model_config, acquire_instruction.round_acquire_method)
+    def __init__(self) -> None:
+        super().__init__()
+    def operate(self, acquire_instruction:SequentialAcConfig, data_splits: DataSplits, old_model_config: OldModelConfig, new_model_config:NewModelConfig):
+        self.sub_strategy = StrategyFactory(old_model_config, new_model_config, acquire_instruction.round_acquire_method)
         ds = deepcopy(data_splits)
-        ds.get_dataloader(self.new_model_config.batch_size)
-        pure = self.new_model_config.pure
+        ds.get_dataloader(new_model_config.batch_size)
+        pure = new_model_config.pure
         minority_cnt = 0
         new_data_total_set = None
         rounds = acquire_instruction.sequential_rounds
 
-        model = load_model(self.old_model_config)
-
         for round_i in range(rounds):
             acquire_instruction.set_round(round_i)
-            new_data_round_indices,_ = self.sub_strategy.get_new_data_indices(acquire_instruction.round_data_per_class, ds)
+            new_data_round_indices,_ = self.sub_strategy.get_new_data_indices(acquire_instruction.round_data_per_class, ds, old_model_config)
             new_data_round_set = torch.utils.data.Subset(ds.dataset['market_aug'],new_data_round_indices)
             new_data_round_set_no_aug = torch.utils.data.Subset(ds.dataset['market'],new_data_round_indices)
 
-            ds.reduce('market', new_data_round_indices, self.new_model_config.batch_size, acquire_instruction)
-            ds.reduce('market_aug', new_data_round_indices, self.new_model_config.batch_size, acquire_instruction)
-            ds.expand('train', new_data_round_set, self.new_model_config.batch_size, acquire_instruction)
-            ds.expand('train_clip', new_data_round_set_no_aug, self.new_model_config.batch_size, acquire_instruction)
+            ds.reduce('market', new_data_round_indices, new_model_config.batch_size, acquire_instruction)
+            ds.reduce('market_aug', new_data_round_indices, new_model_config.batch_size, acquire_instruction)
+            # ds.expand('train', new_data_round_set, new_model_config.batch_size, acquire_instruction)
+            ds.expand('train', new_data_round_set_no_aug, new_model_config.batch_size, acquire_instruction)
+            ds.expand('train_clip', new_data_round_set_no_aug, new_model_config.batch_size, acquire_instruction)
 
-            model = get_new_model(self.new_model_config, ds.loader['train'], ds.loader['val'], model)
-            new_data_total_set = new_data_round_set  if (new_data_total_set == None) else torch.utils.data.ConcatDataset([new_data_total_set,new_data_round_set])
+            model = get_new_model(new_model_config, ds.loader['train'], ds.loader['val'], model)
+            # new_data_total_set = new_data_round_set  if (new_data_total_set == None) else torch.utils.data.ConcatDataset([new_data_total_set,new_data_round_set])
+            new_data_total_set = new_data_round_set_no_aug  if (new_data_total_set == None) else torch.utils.data.ConcatDataset([new_data_total_set,new_data_round_set_no_aug])
 
-        assert len(new_data_total_set) == acquire_instruction.get_new_data_size(self.new_model_config.class_number), 'size error with new data'
+        assert len(new_data_total_set) == acquire_instruction.get_new_data_size(new_model_config.class_number), 'size error with new data'
         if pure:
-            ds.update_dataset('train', new_data_total_set, self.new_model_config.batch_size)
-            model = get_new_model(self.new_model_config, ds.loader['train'], ds.loader['val'], model)
+            ds.update_dataset('train', new_data_total_set, new_model_config.batch_size)
+            model = get_new_model(new_model_config, ds.loader['train'], ds.loader['val'], model)
 
-        log_data(ds.dataset['train'], self.new_model_config, acquire_instruction)
-        self.new_model_config.set_path(acquire_instruction)
-        save_model(model,self.new_model_config.path)
+        log_data(ds.dataset['train'], new_model_config, acquire_instruction)
+        new_model_config.set_path(acquire_instruction)
+        save_model(model,new_model_config.path)
 
-def StrategyFactory(old_model_config, new_model_config,strategy):
+def StrategyFactory(strategy):
     if strategy=='dv':
-        return Greedy(old_model_config, new_model_config)
+        return Greedy()
     elif strategy =='sm':
-        return Sample(old_model_config, new_model_config)
+        return Sample()
     elif strategy == 'conf':
-        return Confidence(old_model_config, new_model_config)
+        return Confidence()
     elif strategy == 'mix':
-        return Mix(old_model_config, new_model_config)
+        return Mix()
     elif strategy == 'seq':
-        return Seq(old_model_config, new_model_config)
+        return Seq()
     else:
-        return SeqCLF(old_model_config, new_model_config)
+        return SeqCLF()
