df56a50d7d47f6d267bcb5adf59ced1755175978
yiwei <e-well@outlook.com>
2023-04-30 23:19:21 -0400

    clarify variable names

--- cifar-100/utils/objects/CLF.py
+++ cifar-100/utils/objects/CLF.py
@@ -33,20 +33,21 @@ def precision(clf, clip_processor, dataloader, base_model):
 class SVM():
     def __init__(self, set_up_data) -> None:
         self.clip_processor = CLIPProcessor(ds_mean=config['data']['mean'], ds_std=config['data']['std'])
-        clip_set_up,_, _ = self.clip_processor.evaluate_clip_images(set_up_data)        
+        set_up_embedding, _ = self.clip_processor.evaluate_clip_images(set_up_data)        
         self.fitter = SVMFitter(method=config['clf'], svm_args=config['clf_args'],cv=config['clf_args']['k-fold'])
-        self.fitter.set_preprocess(clip_set_up) 
+        self.fitter.set_preprocess(set_up_embedding) 
 
     def fit(self, base_model, fit_data):
         svm_fit_gt,svm_fit_pred,_ = Model.evaluate(fit_data,base_model)
-        clip_fit,_ ,_ = self.clip_processor.evaluate_clip_images(fit_data)
-        score = self.fitter.fit(preds=svm_fit_pred, gts=svm_fit_gt, latents=clip_fit)        
+        fit_embedding, fit_gts = self.clip_processor.evaluate_clip_images(fit_data)
+        assert((svm_fit_gt == fit_gts).sum()==len(svm_fit_gt))
+        score = self.fitter.fit(preds=svm_fit_pred, gts=svm_fit_gt, latents=fit_embedding)        
         return score
     
     def predict(self, data_loader, compute_metrics=False, dataset_preds=None):
-        ds_clip, _, clip_gts = self.clip_processor.evaluate_clip_images(data_loader)
-        _, dv, precision = self.fitter.predict(latents=ds_clip, gts=clip_gts, compute_metrics=compute_metrics, preds=dataset_preds)
+        embedding, data_gts = self.clip_processor.evaluate_clip_images(data_loader)
+        _, dv, precision = self.fitter.predict(latents=embedding, gts=data_gts, compute_metrics=compute_metrics, preds=dataset_preds)
         return {
-            'gt': clip_gts,
+            'gt': data_gts,
             'dv': dv,
         }, precision        
