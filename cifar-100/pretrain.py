0e58c25d23fb1f6fe577048fd087dc80a34d8a34
yiwei <e-well@outlook.com>
2023-05-01 15:14:06 -0400

    every epoch/data split

--- cifar-100/pretrain.py
+++ cifar-100/pretrain.py
@@ -13,7 +13,7 @@ def run(ds:Dataset.DataSplits, model_config:Config.OldModel, train_flag:bool):
     shift_score = Model.shift_importance(ds.dataset['test_shift'], model_config.class_number, gt, pred)
     clf = CLF.SVM(ds.loader['train_clip'])
     score = clf.fit(base_model, ds.loader['val_shift'])
-    _, precision = clf.predict(ds.loader['test_shift'])
+    _, precision = clf.predict(ds.loader['test_shift'], compute_metrics=True, base_model=base_model)
     if train_flag:
         # Get a new base model from Resnet
         Model.save(base_model, model_config.path)
@@ -24,20 +24,26 @@ def main(epochs,  model_dir ='', train_flag=False, device=0):
     acc_list, clf_score_list, clf_prec_list, shift_score_list, acc_shift_list = [], [], [], [], []
     for epo in range(epochs):
         print('in epoch {}'.format(epo))
-        ds = ds_list[epo]
-        labels = acquistion.get_loader_labels(ds.loader['train'])
         old_model_config = Config.OldModel(batch_size,superclass_num,model_dir, device_config, epo)
-        acc, score, prec, shift_score, acc_shift = run(ds, old_model_config, train_flag)
+        ds = ds_list[epo]
+        dataset_splits = Dataset.DataSplits(ds, old_model_config.batch_size)
+        acc, score, prec, shift_score, acc_shift = run(dataset_splits, old_model_config, train_flag)
         acc_list.append(acc)
         clf_score_list.append(score)
         clf_prec_list.append(prec)
         shift_score_list.append(shift_score)
         acc_shift_list.append(acc_shift)
-    print('Model Average Acc before shift:', np.round(np.mean(acc_list),decimals=3))
-    print('Model Average Acc after shift:', np.round(np.mean(acc_shift_list),decimals=3))
+        print('Model Acc before shift:', np.round(acc,decimals=3))
+        print('Model Acc after shift:', np.round(acc_shift,decimals=3))
+        print('Distribution Shift Proportion on Model Misclassifications', np.round(shift_score, decimals=3))
+        print(score)
+        print(prec)
+
+    print('Average Acc before shift:', np.round(np.mean(acc_list),decimals=3))
+    print('Average Acc after shift:', np.round(np.mean(acc_shift_list),decimals=3))
     CLF.statistics(clf_score_list, clf_prec_list)
     print('Distribution Shift Proportion on Model Misclassifications', np.round(np.mean(np.array(shift_score_list),axis=0), decimals=3))
-    split_data = ds.dataset
+    split_data = ds
     for spit_name in split_data.keys():
         print(spit_name, len(split_data[spit_name]))
 import argparse
