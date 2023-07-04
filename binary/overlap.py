c7a988855e66b296e69314084a3d0dfdc476c23e
yiwei <e-well@outlook.com>
2023-07-04 16:18:01 -0400

    delete train_clip, detect and stream to acquistion

--- binary/overlap.py
+++ binary/overlap.py
@@ -8,49 +8,39 @@ def remove_old_data(dataset, sample_ratio):
     _, left_data = Dataset.split_dataset(dataset, old_labels, sample_ratio)
     return left_data
 
-def run(ds:Dataset.DataSplits, model_config:Config.OldModel, clip_processor, old_data_ratio, clf_fit_ds, clf_check_ds):
-    if model_config.base_type == 'svm':
-        base_model = Model.svm(ds.loader['train_clip'], clip_processor)
-    else:
-        base_model = Model.resnet(2)
+def run(ds:Dataset.DataSplits, model_config:Config.OldModel, old_data_ratio, clf_fit_ds, clf_check_ds, detect_instruction:Config.Dectector):
+    base_model = Model.prototype_factory(model_config.base_type, model_config.class_number, detect_instruction.vit)
     base_model.load(model_config)
 
     gt,pred,_  = base_model.eval(ds.loader[clf_fit_ds])
     acc_shift = (gt==pred).mean()*100
+    
+    clf = Detector.factory(detect_instruction.name, clip_processor = detect_instruction.vit, split_and_search=True)
+    _ = clf.fit(base_model, ds.loader['val_shift'], ds.dataset['val_shift'], model_config.batch_size)
+    _, clf_acc = clf.predict(ds.loader['val_shift'], compute_metrics=True, base_model=base_model)
+    _, detect_prec = clf.predict(ds.loader['test_shift'], compute_metrics=True, base_model=base_model)
 
-    clf = Detector.resnet(2)
-    clf.fit(base_model, ds.dataset[clf_fit_ds], ds.loader[clf_fit_ds], model_config.batch_size)
-    _, clf_acc = clf.predict(ds.loader[clf_fit_ds], base_model, model_config.batch_size, compute_metrics=True)
-    _, detect_prec = clf.predict(ds.loader[clf_check_ds], base_model, model_config.batch_size, compute_metrics=True)
-    intersection_area = stat_test.run(clf, ds.loader[clf_check_ds], model_config, old_data_ratio, plot=True, overlap_cut_value=0.5)
+    intersection_area = stat_test.run(clf, ds.loader[clf_check_ds], base_model, overlap_cut_value=0.5, model_config=model_config, removal_ratio=old_data_ratio, plot=False)
+    # intersection_area = 0
 
-    # clf = Detector.SVM(ds.loader['train_clip'], clip_processor, split_and_search=True, data_transform='flatten')
-    # _ = clf.fit(base_model, ds.loader[clf_fit_ds])
-    # print('In CLF fit domain:')
-    # _, clf_acc = clf.predict(ds.loader[clf_fit_ds], base_model, compute_metrics=True)
-    # _, detect_prec = clf.predict(ds.loader[clf_check_ds], base_model, compute_metrics=True)
-    # intersection_area = stat_test.run(clf, ds.loader[clf_check_ds], model_config, old_data_ratio, plot=True, overlap_cut_value=0)
-
-    shift_score = Subset.mis_cls_stat(clf_fit_ds, ds, base_model)
-
-    # intersection_area = stat_test.run(clf, ds.loader[clf_check_ds], model_config, old_data_ratio, plot=True, overlap_cut_value=0.5)
+    shift_score = Subset.mis_label_stat(clf_fit_ds, ds, base_model)
 
     return acc_shift, detect_prec, shift_score, intersection_area, clf_acc
 
-def ratio_run(ratio, dataset, old_model_config, clip_processor, clf_fit_ds = 'val_shift', clf_check_ds='test_shift'):
+def ratio_run(ratio, dataset, old_model_config, clf_fit_ds = 'val_shift', clf_check_ds='test_shift', detect_instruction:Config.Dectector=None):
     ds = deepcopy(dataset)
     ds[clf_fit_ds] = remove_old_data(ds[clf_fit_ds], ratio)
     print(clf_fit_ds, len(ds[clf_fit_ds]))
     print(clf_check_ds, len(ds[clf_check_ds]))
 
     ds = Dataset.DataSplits(ds, old_model_config.batch_size)
-    acc_shift, detect_prec, shift_score, intersection_area, clf_acc = run(ds, old_model_config, clip_processor, ratio, clf_fit_ds, clf_check_ds)
+    acc_shift, detect_prec, shift_score, intersection_area, clf_acc = run(ds, old_model_config, ratio, clf_fit_ds, clf_check_ds, detect_instruction)
     return acc_shift, detect_prec, shift_score, intersection_area.tolist()[0]*100, clf_acc
 
-def epoch_run(old_model_config: Config.OldModel, dataset, old_data_ratio_list, clip_processor):
+def epoch_run(old_model_config: Config.OldModel, dataset, old_data_ratio_list, detect_instruction:Config.Dectector):
     acc_epo, acc_shift_epo, detect_prec_epo, shift_epo, intersection_area_epo, clf_acc_epo = [], [], [], [], [], []
     for old_data_ratio in old_data_ratio_list:
-        acc_shift, detect_prec, shift_score, intersection_area, clf_acc = ratio_run(old_data_ratio, dataset, old_model_config, clip_processor)
+        acc_shift, detect_prec, shift_score, intersection_area, clf_acc = ratio_run(old_data_ratio, dataset, old_model_config, detect_instruction=detect_instruction)
         acc_shift_epo.append(acc_shift)
         detect_prec_epo.append(detect_prec)
         shift_epo.append(shift_score)
@@ -58,9 +48,11 @@ def epoch_run(old_model_config: Config.OldModel, dataset, old_data_ratio_list, c
         clf_acc_epo.append(clf_acc)
     return acc_epo, acc_shift_epo, detect_prec_epo, shift_epo, intersection_area_epo, clf_acc_epo
 
-def main(epochs,  model_dir ='', device_id=0, base_type=''):
+def main(epochs,  model_dir ='', device_id=0, base_type='', detector_type=''):
+    print('Dectector:', detector_type)
     batch_size, label_map, new_img_num_list, superclass_num, ratio, seq_rounds_config, ds_list, device_config = set_up(epochs, False, device_id)
     clip_processor = Detector.load_clip(device_config)
+    detect_instrution = Config.Dectector(detector_type, clip_processor)
 
     old_data_ratio_list, _ = np.linspace(0, 1, retstep=True, num=5)
     # old_data_ratio_list = [0]
@@ -70,7 +62,7 @@ def main(epochs,  model_dir ='', device_id=0, base_type=''):
         print('in epoch {}'.format(epo))
         old_model_config = Config.OldModel(batch_size['base'],superclass_num,model_dir, device_config, epo, base_type)
         ds_init = ds_list[epo]
-        acc_epo, acc_shift_epo, detect_prec_epo, shift_epo, intersection_area_epo, clf_acc = epoch_run(old_model_config, ds_init, old_data_ratio_list, clip_processor)
+        acc_epo, acc_shift_epo, detect_prec_epo, shift_epo, intersection_area_epo, clf_acc = epoch_run(old_model_config, ds_init, old_data_ratio_list, detect_instrution)
         acc_list.append(acc_epo)
         acc_shift_list.append(acc_shift_epo)
         detect_prec_list.append(detect_prec_epo)
@@ -98,8 +90,9 @@ if __name__ == '__main__':
     parser.add_argument('-e','--epochs',type=int,default=1)
     parser.add_argument('-md','--model_dir',type=str,default='')
     parser.add_argument('-d','--device',type=int,default=0)
-    parser.add_argument('-bt','--base_type',type=str,default='resnet')
+    parser.add_argument('-bt','--base_type',type=str,default='resnet_1')
+    parser.add_argument('-dn','--detector_name',type=str,default='svm')
 
     args = parser.parse_args()
     # method, img_per_cls, Model.save
-    main(args.epochs,args.model_dir, args.device, args.base_type)
+    main(args.epochs,args.model_dir, args.device, args.base_type, args.detector_name)
