10668a8bd5203232b85ec5e54cf9f503ad6c2ffc
yiwei <e-well@outlook.com>
2023-07-12 11:41:04 -0400

    feat: more methods and args

--- binary/test/acquired_stat.py
+++ binary/test/acquired_stat.py
@@ -2,10 +2,18 @@ import tools
 from utils.strategy import *
 from utils.set_up import set_up
 import utils.statistics.checker as Checker
-import utils.statistics.subset as Subset
 import utils.statistics.stat_test as Plot_stat
 
-def n_data_run(acquisition_config, checker: Checker.subset, data_split, pdf_method):
+def get_cor_incor_dstr(model: Model.prototype, dataloader):
+    '''
+    Correctly Classified Points, Misclassified Points
+    '''
+    dataset_gts, dataset_preds, _ = model.eval(dataloader)
+    cor_mask = (dataset_gts == dataset_preds)
+    incor_mask = ~cor_mask
+    return dataset_gts[cor_mask], dataset_preds[incor_mask]
+
+def n_data_run(acquisition_config, checker: Checker.subset, data_split:Dataset.DataSplits, pdf_method):
     check_result = check_data(acquisition_config, data_split, checker, pdf_method)
     return check_result
 
@@ -17,7 +25,7 @@ def dv_dstr_plot(cor_dv, incor_dv, n_data, pdf_method=None, range=None):
     Plot_stat.plt.close()
     print('Save fig to figure/train/dv{}_{}.png'.format(pdf_name, n_data))
     
-def method_run(n_img_list, acquisition_config:Config.Acquistion, checker: Checker.subset, data_split, pdf_method):
+def method_run(n_img_list, acquisition_config:Config.Acquistion, checker: Checker.subset, data_split:Dataset.DataSplits, pdf_method):
     acc_change_list = []
     for n_img in n_img_list:
         acquisition_config.n_ndata = n_img
@@ -25,7 +33,7 @@ def method_run(n_img_list, acquisition_config:Config.Acquistion, checker: Checke
         acc_change_list.append(acc_change)
     return acc_change_list
 
-def run(acquisition_config:Config.Acquistion, methods, new_img_num_list, checker:Checker.subset, data_split, pdf_method):
+def run(acquisition_config:Config.Acquistion, methods, new_img_num_list, checker:Checker.subset, data_split:Dataset.DataSplits, pdf_method):
     result_list = []
     for method in methods:
         print('In method', method)
@@ -36,20 +44,33 @@ def run(acquisition_config:Config.Acquistion, methods, new_img_num_list, checker
         result_list.append(result_method)
     return result_list
 
-def epoch_run(new_img_num_list, method_list, acquire_instruction:Config.Acquistion, checker: Checker.subset, data_split, pdf_method):
+def epoch_run(new_img_num_list, method_list, acquire_instruction:Config.Acquistion, checker: Checker.subset, data_split:Dataset.DataSplits, pdf_method):
     result_epoch = run(acquire_instruction, method_list, new_img_num_list, checker, data_split, pdf_method)
     return result_epoch
 
-def check_data(acquisition_config:Config.Acquistion, data_split, checker:Checker.subset, pdf_method):
+def check_data(acquisition_config:Config.Acquistion, data_split:Dataset.DataSplits, checker:Checker.subset, pdf_method):
     model_config = checker.model_config
     model_config.set_path(acquisition_config)
     new_data, indices = Log.get_log_data(acquisition_config, model_config, data_split.dataset['market'])
     new_data_loader = torch.utils.data.DataLoader(new_data, batch_size= model_config.new_batch_size)
-    old_labels = set(Subset.config['data']['train_label']) - set(Subset.config['data']['remove_fine_labels'])
-    print(Subset.label_stat(new_data, Subset.config['data']['remove_fine_labels']), Subset.label_stat(new_data, old_labels))
+    
+    # old_labels = set(Subset.config['data']['train_label']) - set(Subset.config['data']['remove_fine_labels'])
+    # print(Subset.label_stat(new_data, Subset.config['data']['remove_fine_labels']), Subset.label_stat(new_data, old_labels))
+    cor, incor = get_cor_incor_dstr(checker.base_model, new_data_loader)
+    return len(incor) / (len(cor) + len(incor)) * 100
+   
+    # train_loader = torch.utils.data.DataLoader(data_split.dataset['train'], batch_size= model_config.new_batch_size)
+    # base_gt, base_pred, _ = checker.base_model.eval(train_loader)
+    # base_incor_mask = (base_gt != base_pred)
+    # base_incor = base_gt[base_incor_mask]
+    # return (len(incor) + len(base_incor)) / (acquisition_config.n_ndata + len(data_split.dataset['train'])) * 100
+
     # cor_dv, incor_dv = Plot_stat.get_dv_dstr(checker.base_model, new_data_loader, checker.clf)
-    # plot_range = (-2.20255523463167, -0.48397627974006063) # test_dv
+    # print('Old model mistakes in acquired data: {}%'.format())
+    # plot_range = (-2.5, 0) # test_dv
     # dv_dstr_plot(cor_dv, incor_dv, acquisition_config.n_ndata, pdf_method, plot_range)
+    
+
 
     # market_dv, _ = checker.clf.predict(data_split.loader['market'], checker.base_model)
     # test_dv, _ = checker.clf.predict(data_split.loader['test_shift'], checker.base_model)
@@ -57,36 +78,38 @@ def check_data(acquisition_config:Config.Acquistion, data_split, checker:Checker
     # new_data_dv, _ = checker.clf.predict(new_data_loader, checker.base_model)
     # ks_result = Plot_stat.kstest(new_data_dv, test_dv)
     # return ks_result.pvalue
-    # incor_pred_stat = Subset.incorrect_pred_stat(new_data_loader, checker.base_model)
-    # print(Subset.new_cls_stat(new_data))
-    # return incor_pred_stat
-
-
-def main(epochs, new_model_setter='retrain', pure=False, model_dir ='', device=0):
-    print('Use pure: ',pure)
+    
+def main(epochs, new_model_setter='retrain', pure=False, model_dir ='', device=0, probab_bound = 0.5, base_type='', detector_name = ''):
     device_config = 'cuda:{}'.format(device)
+    if pure == False:
+        probab_bound = 0
     torch.cuda.set_device(device_config)
-    batch_size, label_map, new_img_num_list, superclass_num, ratio, seq_rounds_config, ds_list, device_config = set_up(epochs, False, device)
+    batch_size, label_map, new_img_num_list, superclass_num, ratio, seq_rounds_config, ds_list, device_config = set_up(epochs, device)
 
     # method_list, method_labels = ['dv','sm','conf','seq_clf'], ['greedy decision value','random sampling','model confidence','sequential with only SVM updates']
-    method_list, method_labels = ['dv'], ['dv']
-    # method_list = ['dv','sm','conf','mix']
-    # method_labels = ['greedy decision value','random sampling','model confidence','greedy+sampling']
+    # method_list = ['dv','sm','conf'] if new_model_setter!='refine' else ['dv']
+    method_list = ['dv','sm','conf']
 
     clip_processor = Detector.load_clip(device_config)
-    parse_para = (batch_size, superclass_num, model_dir, device_config, pure, new_model_setter, seq_rounds_config)
-    acquire_instruction = Config.AcquistionFactory('seq',seq_rounds_config) 
-    stream_instruction = Config.ProbabStream(bound=0.7, pdf='kde')
-
+    detect_instruction = Config.Dectector(detector_name, clip_processor)
+    parse_para = (batch_size, superclass_num,model_dir, device_config, base_type, pure, new_model_setter, seq_rounds_config)
+    acquire_instruction = Config.AcquistionFactory('non_seq',seq_rounds_config) 
+    stream_instruction = Config.ProbabStream(bound=probab_bound, pdf='kde')
+    acquire_instruction.add_detector(detect_instruction)
+    acquire_instruction.add_streaming(stream_instruction)
     results = []
     for epo in range(epochs):
         print('in epoch {}'.format(epo))
-        checker = tools.get_probab_checker(epo, parse_para, ds_list[epo], clip_processor, stream_instruction, plot=False)
+        checker = tools.get_probab_checker(epo, parse_para, ds_list[epo], stream_instruction, detect_instruction, plot=False)
         data_splits = Dataset.DataSplits(ds_list[0], 10)
         result_epoch = epoch_run(new_img_num_list, method_list, acquire_instruction, checker, data_splits, stream_instruction.pdf)
         results.append(result_epoch)
     
-    print(results)
+    result = np.array(results)
+    for idx, method in enumerate(method_list):
+        method_result = result[:, idx, :]
+        print(method)
+        print(*np.round(np.mean(method_result, axis=0), decimals=3).tolist(), sep=',')
 
 import argparse
 if __name__ == '__main__':
@@ -96,6 +119,9 @@ if __name__ == '__main__':
     parser.add_argument('-p','--pure',type=Config.str2bool,default=True)
     parser.add_argument('-md','--model_dir',type=str,default='')
     parser.add_argument('-d','--device',type=int,default=0)
+    parser.add_argument('-bt','--base_type',type=str,default='resnet_1')
+    parser.add_argument('-pb','--probab_bound', type=Config.str2float, default=0.5)
+    parser.add_argument('-dn','--detector_name',type=str,default='svm')
 
     args = parser.parse_args()
-    main(args.epochs,pure=args.pure,model_dir=args.model_dir, device=args.device)
+    main(args.epochs,pure=args.pure,model_dir=args.model_dir, device=args.device, probab_bound=args.probab_bound, base_type=args.base_type, detector_name=args.detector_name)
