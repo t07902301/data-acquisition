9027f07eecc383262c0bb851518a6e74ce883a73
yiwei <e-well@outlook.com>
2023-07-16 17:05:53 -0400

    fix: ensemble from subset, distribution stat

--- binary/utils/statistics/checker.py
+++ binary/utils/statistics/checker.py
@@ -7,29 +7,41 @@ import utils.objects.model as Model
 import utils.objects.Config as Config
 import utils.objects.Detector as Detector
 import utils.objects.dataset as Dataset
-import utils.log as log
-import utils.statistics.stat_test as Plot_Stat
+import utils.log as Log
+import utils.statistics.distribution as Distribution
 from abc import abstractmethod
 import numpy as np
 import torch
 import utils.statistics.subset as Subset
+
 class prototype():
+    '''
+    Decide what to test the new and the old model
+    '''
     def __init__(self,model_config:Config.NewModel) -> None:
         self.model_config = model_config
     @abstractmethod
     def run(self,acquisition_config:Config.Acquistion):
+        '''
+        Load the new model and perform testing
+        '''
         pass
     @abstractmethod
-    def setup(self, old_model_config:Config.OldModel, data_splits):
+    def setup(self, old_model_config: Config.OldModel, datasplits: Dataset.DataSplits, detector_instruction: Config.Dectector, stream_instruction: Config.ProbabStream, plot: bool):
         '''
-        Use the old model and data split to set up a prototype (for each epoch)
+        Use the old model and data split to set up a prototype (for each epoch) -> base_model, clf/detector, anchor loader
         '''
-        pass
+        self.base_model = Model.prototype_factory(old_model_config.base_type, old_model_config.class_number, detector_instruction.vit)
+        self.base_model.load(old_model_config.path, old_model_config.device)
+        self.clf = Detector.factory(detector_instruction.name, clip_processor = detector_instruction.vit, split_and_search=True)
+        _ = self.clf.fit(self.base_model, datasplits.loader['val_shift'])
+        self.anchor_loader = datasplits.loader['val_shift']
+
 class DV(prototype):
     def __init__(self, model_config: Config.NewModel) -> None:
         super().__init__(model_config)
         # prototype.__init__(self, model_config) # TODO: a problem in the constructor of multi-inheritence MRO
-        self.log_config = log.get_config(model_config)
+        self.log_config = Log.get_config(model_config)
     def load(self):
         data = torch.load(self.log_config.path)
         print('load DV from {}'.format(self.log_config.path))
@@ -55,6 +67,7 @@ class benchmark(DV):
         data_info, _ = Detector.apply_CLF(self.clf, datasplits.loader['market'], self.clip_processor, self.base_model)
         # return (data_info['dv']<0).sum()/len(data_info['dv'])     
         return np.std(data_info['dv']), np.mean(data_info['dv'])
+
 class subset(prototype):
     '''
     Split test set by a dv threshold or model mistakes, and then feed test splits into models. \n
@@ -65,10 +78,10 @@ class subset(prototype):
         super().__init__(model_config)
     
     def setup(self, old_model_config:Config.OldModel, datasplits:Dataset.DataSplits, detector_instruction:Config.Dectector):
-        self.base_model = Model.prototype_factory(old_model_config.base_type, old_model_config.class_number, detector_instruction.vit)
-        self.base_model.load(old_model_config.path, old_model_config.device)
-        self.clf = Detector.factory(detector_instruction.name, clip_processor = detector_instruction.vit, split_and_search=True)
-        _ = self.clf.fit(self.base_model, datasplits.loader['val_shift'])
+        '''
+        set base model, test data info and vit
+        '''
+        super().setup(old_model_config, datasplits, detector_instruction)
         gt, pred, _ = self.base_model.eval(datasplits.loader['test_shift'])
         self.base_acc = (gt == pred).mean()*100 
         self.test_info = Subset.build_data_info(datasplits, 'test_shift', self.clf, self.model_config, self.base_model)
@@ -120,42 +133,39 @@ class probability(subset):
     def __init__(self, model_config: Config.NewModel) -> None:
         super().__init__(model_config)  
 
-    def get_dv_dstr(self, dataloader, pdf_type):
-        '''
-        Get decision value distribution of a dataloader under a base model
-        '''
-        cor_dv, incor_dv = Plot_Stat.get_dv_dstr(self.base_model, dataloader, self.clf)
-        # total_dv = np.concatenate((incor_dv,cor_dv))
-        # print('Negative DV: {}%'.format((total_dv<0).mean()*100))
-        correct_prior = (len(cor_dv)) / (len(cor_dv) + len(incor_dv))
-        correct_dstr = Subset.disrtibution(correct_prior, Plot_Stat.get_pdf(cor_dv, pdf_type))
-        incorrect_dstr =  Subset.disrtibution(1 - correct_prior, Plot_Stat.get_pdf(incor_dv, pdf_type))
-        return correct_dstr, incorrect_dstr
+    def get_subset_loader(self, stream_instruction:Config.Stream, set_up_loader):
+        correct_dstr, incorrect_dstr = Distribution.get_dv_dstr(set_up_loader, stream_instruction.pdf)
+        loader, selected_probab = Subset.probability_setter().get_subset_loders(self.test_info, correct_dstr, incorrect_dstr, stream_instruction)
+        return loader, selected_probab
 
     def setup(self, old_model_config:Config.OldModel, datasplits:Dataset.DataSplits, detector_instruction: Config.Dectector, stream_instruction:Config.ProbabStream, plot:bool):
         super().setup(old_model_config, datasplits, detector_instruction) 
-        correct_dstr, incorrect_dstr = self.get_dv_dstr(datasplits.loader['val_shift'], stream_instruction.pdf)
-        self.loader, selected_probab = Subset.probability_setter().get_subset_loders(self.test_info, correct_dstr, incorrect_dstr, stream_instruction)
-        cor_dv, incor_dv = Plot_Stat.get_dv_dstr(self.base_model, self.loader['new_model'], self.clf)
-        print('Hard images in New Test: {}%'.format(len(incor_dv) / (len(incor_dv) + len(cor_dv)) * 100))
+        self.test_loader, selected_probab = self.get_subset_loader(stream_instruction, self.anchor_loader)
+        cor_dv, incor_dv = Subset.get_hard_easy_dv(self.base_model, self.test_loader['new_model'], self.clf)
+        print('Setup - Hard images in New Test: {}%'.format(len(incor_dv) / (len(incor_dv) + len(cor_dv)) * 100))
         if plot:
             pdf_name = self.get_pdf_name(stream_instruction.pdf)
             fig_name = 'figure/test/probab.png'
             self.probab_dstr_plot(selected_probab, fig_name)
             fig_name = 'figure/test/dv.png'
-            cor_dv, incor_dv = Plot_Stat.get_dv_dstr(self.base_model, datasplits.loader['test_shift'], self.clf)
+            cor_dv, incor_dv = Subset.get_hard_easy_dv(self.base_model, datasplits.loader['test_shift'], self.clf)
             self.dv_dstr_plot(cor_dv, incor_dv, fig_name, stream_instruction.pdf)
             fig_name = 'figure/test/selected_{}{}.png'.format(stream_instruction.bound, pdf_name)
-            self.selected_dstr_plot(self.loader['new_model'], fig_name, stream_instruction.pdf)
+            self.selected_dstr_plot(self.test_loader['new_model'], fig_name, stream_instruction.pdf)
 
     def selected_dstr_plot(self, selected_loader, fig_name, pdf=None):
-        cor_dv, incor_dv = Plot_Stat.get_dv_dstr(self.base_model, selected_loader, self.clf)
+        cor_dv, incor_dv = Subset.get_hard_easy_dv(self.base_model, selected_loader, self.clf)
         print('Old model mistakes in selected test: {}%'.format(len(incor_dv) / (len(incor_dv) + len(cor_dv)) * 100))
         self.dv_dstr_plot(cor_dv, incor_dv, fig_name, pdf)
 
     def run(self, acquisition_config:Config.Acquistion):
+        if 'seq' in acquisition_config.method:
+            self.clf = Log.get_log_clf(acquisition_config, self.model_config)
+            self.test_loader, _ = self.get_subset_loader(acquisition_config.stream, self.anchor_loader)
+            cor_dv, incor_dv = Subset.get_hard_easy_dv(self.base_model, self.test_loader['new_model'], self.clf)
+            print('Running - Hard images in New Test: {}%'.format(len(incor_dv) / (len(incor_dv) + len(cor_dv)) * 100))
         self.model_config.set_path(acquisition_config)
-        return self._target_test(self.loader)
+        return self._target_test(self.test_loader)
 
     def _target_test(self, loader):
         return super()._target_test(loader)
@@ -167,48 +177,41 @@ class probability(subset):
         test_loader = torch.utils.data.DataLoader(self.test_info['dataset'], batch_size=self.model_config.batch_size)
         dataset_gts, dataset_preds, _ = self.base_model.eval(test_loader)
         correct_mask = (dataset_gts == dataset_preds)
-        Plot_Stat.base_plot(probab[correct_mask], 'correct', 'green', pdf_method)        
+        Distribution.base_plot(probab[correct_mask], 'correct', 'green', pdf_method)        
         incorrect_mask = ~correct_mask
-        Plot_Stat.base_plot(probab[incorrect_mask], 'incorrect', 'red', pdf_method)  
-        Plot_Stat.plt.xlabel('Probability')
-        Plot_Stat.plt.savefig(fig_name)
-        Plot_Stat.plt.close()
+        Distribution.base_plot(probab[incorrect_mask], 'incorrect', 'red', pdf_method)  
+        Distribution.plt.xlabel('Probability')
+        Distribution.plt.savefig(fig_name)
+        Distribution.plt.close()
         print('Save fig to {}'.format(fig_name))
 
     def dv_dstr_plot(self, cor_dv, incor_dv, fig_name, pdf_method=None):
-        Plot_Stat.base_plot(cor_dv, 'correct', 'orange', pdf_method)
-        Plot_Stat.base_plot(incor_dv, 'incorrect', 'blue', pdf_method)
-        # Plot_Stat.plt.savefig()
-        Plot_Stat.plt.xlabel('Decision Value')
-        Plot_Stat.plt.ylabel('Density')
-        Plot_Stat.plt.savefig(fig_name)
-        Plot_Stat.plt.close()
+        Distribution.base_plot(cor_dv, 'correct', 'orange', pdf_method)
+        Distribution.base_plot(incor_dv, 'incorrect', 'blue', pdf_method)
+        # Distribution.plt.savefig()
+        Distribution.plt.xlabel('Decision Value')
+        Distribution.plt.ylabel('Density')
+        Distribution.plt.savefig(fig_name)
+        Distribution.plt.close()
         print('Save fig to {}'.format(fig_name))
 
-class ensemble(probability):
+class ensemble(subset):
     def __init__(self, model_config: Config.NewModel) -> None:
         super().__init__(model_config)
+    
     def setup(self, old_model_config: Config.OldModel, datasplits: Dataset.DataSplits, detector_instruction: Config.Dectector, stream_instruction: Config.ProbabStream, plot: bool):
-        self.base_model = Model.prototype_factory(old_model_config.base_type, old_model_config.class_number, detector_instruction.vit)
-        self.base_model.load(old_model_config.path, old_model_config.device)
-        self.clf = Detector.factory(detector_instruction.name, clip_processor = detector_instruction.vit, split_and_search=True)
-        _ = self.clf.fit(self.base_model, datasplits.loader['val_shift'])
-        gt, pred, _ = self.base_model.eval(datasplits.loader['test_shift'])
-        self.base_acc = (gt == pred).mean() * 100 
+        super().setup(old_model_config, datasplits, detector_instruction)
         self.pdf_type = stream_instruction.pdf
-        self.loader = datasplits.loader['test_shift']
         self.probab_setter = Subset.probability_setter()
-        self.vit = detector_instruction.vit
-        self.anchor_loader = datasplits.loader['val_shift']
 
     def get_dstr_weights(self, dataloader, pdf_type):
-        correct_dstr, incorrect_dstr = self.get_dv_dstr(dataloader, pdf_type)
+        correct_dstr, incorrect_dstr = Distribution.get_dv_dstr(dataloader, pdf_type)
         old_weights, new_weights = [], []
         dv, _ = self.clf.predict(dataloader, self.base_model)        
         for value in dv:
-            new_probab = self.probab_setter.get_probab(value, incorrect_dstr, correct_dstr, self.pdf_type)
+            new_probab = self.probab_setter.get_probab(value, incorrect_dstr, correct_dstr, pdf_type)
             new_weights.append(new_probab)
-            old_probab = self.probab_setter.get_probab(value, correct_dstr, incorrect_dstr, self.pdf_type)
+            old_probab = self.probab_setter.get_probab(value, correct_dstr, incorrect_dstr, pdf_type)
             old_weights.append(old_probab)
             # assert old_probab[0] + new_probab[0] == 1, old_probab[0] + new_probab[0] 
         return np.concatenate(old_weights), np.concatenate(new_weights)
@@ -219,7 +222,8 @@ class ensemble(probability):
         gts, _, new_probab  = new_model.eval(dataloader)
         _, _, old_probab  = self.base_model.eval(dataloader)  
         preds = np.zeros(len(gts))
-        probab = self.average(new_probab, old_probab)
+        # probab = self.average(new_probab, old_probab)
+        probab = self.weight_dstr(dataloader, new_probab, old_probab)
         # probab = self.weight_error(new_probab, old_probab, new_model, self.base_model, self.anchor_loader)
         if self.model_config.class_number == 1:
             preds[probab >= 0.5] = 1
@@ -249,6 +253,17 @@ class ensemble(probability):
         old_alpha = self.get_boosting_alpha(old_model, anchor_dataloader)
         probab = new_probab * new_alpha + old_probab * old_alpha
         return probab
+
+def factory(name, new_model_config):
+    if name == 'subset':
+        checker = subset(new_model_config)
+    elif name == 'probab':
+        checker = probability(new_model_config)
+    elif name == 'ensemble':
+        checker = ensemble(new_model_config)
+    else:
+        checker = prototype(new_model_config)
+    return checker
 
 class total(prototype):
     def __init__(self, model_config: Config.NewModel) -> None:
