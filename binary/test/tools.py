f5b5e10831f353fb5ac3dc9354adb232c726eb57
yiwei <e-well@outlook.com>
2023-07-16 17:08:00 -0400

    fix: get checker, seq clf to checker.run

--- binary/test/tools.py
+++ binary/test/tools.py
@@ -8,8 +8,6 @@ def method_run(n_img_list, acquisition_config:Config.Acquistion, checker: Checke
     acc_change_list = []
     for n_img in n_img_list:
         acquisition_config.n_ndata = n_img
-        if 'seq' in acquisition_config.method:
-            checker.clf = Log.get_log_clf(acquisition_config, checker.model_config)
         acc_change = n_data_run(acquisition_config, checker)
         acc_change_list.append(acc_change)
     return acc_change_list
@@ -40,29 +38,15 @@ def epoch_run(new_img_num_list, method_list, acquire_instruction:Config.Acquisti
     # bound_stat = check_bound(old_model_config, dataset_splits,acquire_instruction, clip_processor)
     return result_epoch, 0
 
-def get_probab_checker(epoch, parse_para, dataset, stream_instruction:Config.Stream, detect_instruction: Config.Dectector, plot=True):
-    batch_size, superclass_num,model_dir, device_config, base_type, pure, new_model_setter, seq_rounds_config = parse_para
+def get_checker_args(epoch, parse_args, dataset):
+    batch_size, superclass_num,model_dir, device_config, base_type, pure, new_model_setter, seq_rounds_config = parse_args
     old_model_config = Config.OldModel(batch_size['base'], superclass_num, model_dir, device_config, epoch, base_type=base_type)
     new_model_config = Config.NewModel(batch_size['base'], superclass_num, model_dir, device_config, epoch, pure, new_model_setter, batch_size['new'], base_type=base_type)
     dataset_splits = Dataset.DataSplits(dataset, old_model_config.batch_size)
-    checker = Checker.probability(new_model_config) 
+    return old_model_config, new_model_config, dataset_splits
+
+def get_checker(epoch, parse_args, dataset, stream_instruction:Config.Stream, detect_instruction: Config.Dectector, plot=True):
+    old_model_config, new_model_config, dataset_splits = get_checker_args(epoch, parse_args, dataset)
+    checker = Checker.factory(stream_instruction.name, new_model_config)
     checker.setup(old_model_config, dataset_splits, detect_instruction, stream_instruction, plot)
     return checker
-
-def get_ensemble_checker(epoch, parse_para, dataset, stream_instruction:Config.Stream, detect_instruction: Config.Dectector, plot=True):
-    batch_size, superclass_num,model_dir, device_config, base_type, pure, new_model_setter, seq_rounds_config = parse_para
-    old_model_config = Config.OldModel(batch_size['base'], superclass_num, model_dir, device_config, epoch, base_type=base_type)
-    new_model_config = Config.NewModel(batch_size['base'], superclass_num, model_dir, device_config, epoch, pure, new_model_setter, batch_size['new'], base_type=base_type)
-    dataset_splits = Dataset.DataSplits(dataset, old_model_config.batch_size)
-    checker = Checker.ensemble(new_model_config) 
-    checker.setup(old_model_config, dataset_splits, detect_instruction, stream_instruction, plot)
-    return checker
-
-# def get_subset_checker(epoch, parse_para, dataset, stream_instruction:Config.Stream, plot=True):
-#     batch_size, superclass_num,model_dir, device_config, base_type, pure, new_model_setter, seq_rounds_config = parse_para
-#     old_model_config = Config.OldModel(batch_size['base'], superclass_num, model_dir, device_config, epoch, base_type=base_type)
-#     new_model_config = Config.NewModel(batch_size['base'], superclass_num, model_dir, device_config, epoch, pure, new_model_setter, batch_size['new'], base_type=base_type)
-#     dataset_splits = Dataset.DataSplits(dataset, old_model_config.batch_size)
-#     checker = Checker.subset(new_model_config) 
-#     checker.setup(old_model_config, dataset_splits, stream_instruction, plot)
-#     return checker
