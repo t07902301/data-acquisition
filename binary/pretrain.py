ec1218f5288f07d3957964121a2ccf77e54d4b6d
yiwei <e-well@outlook.com>
2023-07-18 15:56:18 -0400

    feat: compare clf in val and test

--- binary/pretrain.py
+++ binary/pretrain.py
@@ -1,7 +1,7 @@
 from utils.strategy import *
 from utils.set_up import set_up
 import utils.statistics.subset as Subset
-def run(ds:Dataset.DataSplits, model_config:Config.OldModel, train_flag:bool, detect_instruction:Config.Dectector):
+def run(ds:Dataset.DataSplits, model_config:Config.OldModel, train_flag:bool, detect_instruction:Config.Detection):
     base_model = Model.prototype_factory(model_config.base_type, model_config.class_number, detect_instruction.vit)
     if train_flag:
         if model_config.base_type == 'svm':
@@ -12,19 +12,19 @@ def run(ds:Dataset.DataSplits, model_config:Config.OldModel, train_flag:bool, de
         base_model.load(model_config.path, model_config.device)
     # Evaluate
     gt,pred,conf  = base_model.eval(ds.loader['test'])
-    # print(gt[:10])
-    # print(pred[:10])
-    # print(conf[:10])
     acc = (gt==pred).mean()*100
     gt,pred,_  = base_model.eval(ds.loader['test_shift'])
     acc_shift = (gt==pred).mean()*100
-    # if train_flag:
-    #     base_model.save(model_config.path)
-    clf = Detector.factory(detect_instruction.name, clip_processor = detect_instruction.vit, split_and_search=True)
+    clf = Detector.factory(detect_instruction.name, clip_processor = detect_instruction.vit, split_and_search=True, data_transform='clip')
     _ = clf.fit(base_model, ds.loader['val_shift'], ds.dataset['val_shift'], model_config.batch_size)
     _, detect_prec = clf.predict(ds.loader['val_shift'], compute_metrics=True, base_model=base_model)
     print('In fitting CLF:', detect_prec)
+    gt,pred,_  = base_model.eval(ds.loader['val_shift'])
+    print('Val Shift Acc:', (gt==pred).mean()*100)
     _, detect_prec = clf.predict(ds.loader['test_shift'], compute_metrics=True, base_model=base_model)
+    print('In testing CLF:', detect_prec)
+    print('Test Shift Acc:', acc_shift)
+
     if train_flag:
         base_model.save(model_config.path)
 
@@ -37,7 +37,7 @@ def main(epochs,  model_dir ='', train_flag=False, device_id=0, base_type='', de
     batch_size, label_map, new_img_num_list, superclass_num, ratio, seq_rounds_config, ds_list, device_config = set_up(epochs, device_id)
     acc_list, acc_shift_list, detect_prec_list, shift_list = [], [], [], []
     clip_processor = Detector.load_clip(device_config)
-    detect_instrution = Config.Dectector(detector_name, clip_processor)
+    detect_instrution = Config.Detection(detector_name, clip_processor)
     for epo in range(epochs):
         print('in epoch {}'.format(epo))
         old_model_config = Config.OldModel(batch_size['base'],superclass_num,model_dir, device_config, epo, base_type)
@@ -68,5 +68,4 @@ if __name__ == '__main__':
     parser.add_argument('-dn','--detector_name',type=str,default='svm')
 
     args = parser.parse_args()
-    # method, img_per_cls, Model.save
     main(args.epochs,args.model_dir,args.train_flag, args.device, args.base_type, args.detector_name)
