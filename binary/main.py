7e317ed6016920483eae8339457dbbcd0a885b9a
yiwei <e-well@outlook.com>
2023-07-18 12:08:26 -0400

    fix: operation, sm & conf no-detector

--- binary/main.py
+++ binary/main.py
@@ -1,54 +1,59 @@
 from utils.strategy import *
 from utils.set_up import set_up
 
-def n_data_run(acquire_instruction: Config.Acquistion, old_model_config:Config.OldModel, new_model_config:Config.NewModel, dataset:dict):
-    strategy = StrategyFactory(acquire_instruction.method, old_model_config)
-    strategy.operate(acquire_instruction, dataset, new_model_config)
+def n_data_run(operation: Config.Operation, old_model_config:Config.OldModel, new_model_config:Config.NewModel, dataset:dict):
+    strategy = StrategyFactory(operation.acquisition.method, old_model_config)
+    strategy.operate(operation, dataset, new_model_config)
 
-def method_run(new_img_num_list, acquire_instruction: Config.Acquistion, old_model_config:Config.OldModel, new_model_config:Config.NewModel, dataset:dict):
+def method_run(new_img_num_list, operation: Config.Operation, old_model_config:Config.OldModel, new_model_config:Config.NewModel, dataset:dict):
     for new_img_num in new_img_num_list:
-        acquire_instruction.n_ndata = new_img_num
-        n_data_run(acquire_instruction, old_model_config, new_model_config, dataset)
+        operation.acquisition.n_ndata = new_img_num
+        n_data_run(operation, old_model_config, new_model_config, dataset)
 
-def run(dataset:dict, methods_list, new_img_num_list, old_model_config:Config.OldModel, new_model_config:Config.NewModel, acquire_instruction:Config.Acquistion):
+def run(dataset:dict, methods_list, new_img_num_list, old_model_config:Config.OldModel, new_model_config:Config.NewModel, operation: Config.Operation):
     for method in methods_list:
-        acquire_instruction.method = method
-        method_run(new_img_num_list, acquire_instruction, old_model_config, new_model_config, dataset)
+        operation.acquisition.method = method
+        operation.acquisition = Config.AcquisitionFactory(method, operation.acquisition)
+        method_run(new_img_num_list, operation, old_model_config, new_model_config, dataset)
 
-def epoch_run(parse_para, method_list, n_data_list, dataset, acquire_instruction, epo):
+def epoch_run(parse_para, method_list, n_data_list, dataset, epo, operation: Config.Operation):
     batch_size, superclass_num,model_dir, device_config, base_type, pure, new_model_setter, seq_rounds_config = parse_para
     old_model_config = Config.OldModel(batch_size['base'], superclass_num, model_dir, device_config, epo, base_type)
     new_model_config = Config.NewModel(batch_size['base'], superclass_num, model_dir, device_config, epo, pure, new_model_setter, batch_size['new'], base_type)
-    run(dataset, method_list, n_data_list, old_model_config,new_model_config, acquire_instruction)
+    run(dataset, method_list, n_data_list, old_model_config,new_model_config, operation)
 
-def bound_run(parse_para, epochs, ds_list, strategy:str, method_list, new_img_num_list, bound, acquire_instruction: Config.Acquistion):
-    acquire_instruction.bound = bound
+def bound_run(parse_para, epochs, ds_list, method_list, new_img_num_list, bound, operation: Config.Operation):
+    operation.acquisition.bound = bound
     for epo in range(epochs):
         print('In epoch {}'.format(epo))
         dataset = ds_list[epo]
-        epoch_run(parse_para, method_list, new_img_num_list, dataset, acquire_instruction, epo)
+        epoch_run(parse_para, method_list, new_img_num_list, dataset, epo, operation)
 
-def main(epochs, new_model_setter='retrain', pure=False, model_dir ='', strategy='', device=0, base_type='', detector_name=''):
-    print('Detector Name:', detector_name)
-    clip_processor = Detector.load_clip(device)
-    detect_instruction = Config.Dectector(detector_name, clip_processor)
-    probab_bound = 0.5 if pure else 0
-    stream_instruction = Config.ProbabStream(bound=probab_bound, pdf='kde')
-
-    acquire_instruction = Config.AcquistionFactory(strategy, sequential_rounds_config=0)
-    acquire_instruction.add_detector(detect_instruction)
-    acquire_instruction.add_streaming(stream_instruction)
-
+def main(epochs, new_model_setter='retrain', pure=False, model_dir ='', device=0, base_type='', detector_name = ''):
+    print('Detector:', detector_name)
+    device_config = 'cuda:{}'.format(device)
+    torch.cuda.set_device(device_config)
     batch_size, label_map, new_img_num_list, superclass_num, ratio, seq_rounds_config, ds_list, device_config = set_up(epochs, device)
 
-    # method_list = ['dv','sm','conf'] if strategy=='non_seq' else [strategy]
-    method_list = ['dv']
+    probab_bound = 0.5 if pure else 0
+    clip_processor = Detector.load_clip(device_config)
+    stream_instruction = Config.ProbabStream(bound=probab_bound, pdf='kde', name='probab')
+    detect_instruction = Config.Detection(detector_name, clip_processor)
+    acquire_instruction = Config.Acquisition()
+    operation = Config.Operation(acquire_instruction, stream_instruction, detect_instruction)
+
+    if new_model_setter!='refine':
+        # method_list = ['dv','seq_clf']
+        method_list = ['sm','conf']
+    else:
+        assert new_model_setter == 'refine' and pure == False
+        method_list = ['dv']
+    # method_list = ['dv','sm','conf', 'seq_clf'] 
+    # method_list = ['dv']
     bounds = [None]
     for bound in bounds:
         parse_para = (batch_size, superclass_num,model_dir, device_config, base_type, pure, new_model_setter, seq_rounds_config)
-        bound_run(parse_para, epochs, ds_list, strategy, method_list, new_img_num_list, bound, acquire_instruction)
-
-
+        bound_run(parse_para, epochs, ds_list, method_list, new_img_num_list, bound, operation)
 
 import argparse
 if __name__ == '__main__':
@@ -61,7 +66,7 @@ if __name__ == '__main__':
     parser.add_argument('-d','--device',type=int,default=0)
     parser.add_argument('-bt','--base_type',type=str,default='resnet_1')
     parser.add_argument('-dn','--detector_name',type=str,default='svm')
+    parser.add_argument('-ns','--new_model_setter',type=str,default='retrain')
+
     args = parser.parse_args()
-    # method, new_img_num, save_model
-    main(args.epochs,pure=args.pure,model_dir=args.model_dir,strategy=args.strategy,device=args.device, base_type=args.base_type, detector_name=args.detector_name)
-    # print(args.method)
+    main(args.epochs,pure=args.pure,model_dir=args.model_dir, device=args.device, base_type=args.base_type, detector_name=args.detector_name, new_model_setter=args.new_model_setter)
