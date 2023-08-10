cd55cd1306ef5023ee7111d0bcf929a5ff9097a7
yiwei <e-well@outlook.com>
2023-08-10 13:43:33 -0400

    feat: make config a local variable, justify val

--- binary/pretrain.py
+++ binary/pretrain.py
@@ -1,49 +1,53 @@
 from utils.strategy import *
 from utils.set_up import *
 import utils.statistics.data as DataStat
-def run(ds:Dataset.DataSplits, model_config:Config.OldModel, train_flag:bool, detect_instruction:Config.Detection):
+def run(ds:Dataset.DataSplits, model_config:Config.OldModel, train_flag:bool, detect_instruction:Config.Detection, config):
     
-    base_model = Model.factory(model_config.base_type, model_config.class_number, detect_instruction.vit)
+    base_model = Model.factory(model_config.base_type, config, detect_instruction.vit)
 
     if train_flag:
-        base_model.train(ds.loader['train'], ds.loader['val'])
+        if model_config.base_type == 'cnn':
+            base_model.train(ds.loader['train'], ds.loader['val'])
+        else:
+            base_model.train(ds.loader['train_non_cnn'])
     else:
         base_model.load(model_config.path, model_config.device)
     
     # Evaluate
     acc = base_model.acc(ds.loader['test'])
     acc_shift = base_model.acc(ds.loader['test_shift'])
-    clf = Detector.factory(detect_instruction.name, clip_processor = detect_instruction.vit, split_and_search=True, data_transform='clip')
+
+    clf = Detector.factory(detect_instruction.name, config, clip_processor = detect_instruction.vit, split_and_search=True, data_transform='clip')
     clf.fit(base_model, ds.loader['val_shift'], ds.dataset['val_shift'], model_config.batch_size)
+    
     _, detect_prec = clf.predict(ds.loader['val_shift'], compute_metrics=True, base_model=base_model)
     print('In fitting CLF:', detect_prec)
-    gt,pred,_  = base_model.eval(ds.loader['val_shift'])
-    print('Val Shift Acc:', (gt==pred).mean()*100)
+    print('Val Shift Acc:', acc_shift)
+
     _, detect_prec = clf.predict(ds.loader['test_shift'], compute_metrics=True, base_model=base_model)
     print('In testing CLF:', detect_prec)
-    print('Test Shift Acc:', acc_shift)
 
     if train_flag:
         base_model.save(model_config.path)
 
-    task_id = model_config.model_dir[:3]
-    check_labels = Dataset.data_config['remove_fine_labels'][task_id]
-    shift_score = DataStat.mis_label_stat('val_shift', ds, base_model, check_labels)
+    check_labels = config['data']['remove_fine_labels']
+
+    shift_score = DataStat.mis_label_stat('test_shift', ds, base_model, check_labels)
 
     return acc, acc_shift, detect_prec, shift_score
 
 def main(epochs,  model_dir ='', train_flag=False, device_id=0, base_type='', detector_name=''):
     print('Detector Name:', detector_name)
-    batch_size, new_img_num_list, superclass_num, seq_rounds_config, device_config, ds_list, normalize_stat = set_up(epochs, model_dir, device_id)
+    config, device_config, ds_list, normalize_stat = set_up(epochs, model_dir, device_id)
     acc_list, acc_shift_list, detect_prec_list, shift_list = [], [], [], []
     clip_processor = Detector.load_clip(device_config, normalize_stat['mean'], normalize_stat['std'])
     detect_instrution = Config.Detection(detector_name, clip_processor)
     for epo in range(epochs):
         print('in epoch {}'.format(epo))
-        old_model_config = Config.OldModel(batch_size['base'],superclass_num,model_dir, device_config, epo, base_type)
+        old_model_config = Config.OldModel(config['hparams']['batch_size']['base'], config['hparams']['superclass'], model_dir, device_config, epo, base_type)
         ds = ds_list[epo]
         ds = Dataset.DataSplits(ds, old_model_config.batch_size)
-        prec, acc_shift, detect_prec, shift_score = run(ds, old_model_config, train_flag, detect_instrution)
+        prec, acc_shift, detect_prec, shift_score = run(ds, old_model_config, train_flag, detect_instrution, config)
         acc_list.append(prec)
         acc_shift_list.append(acc_shift)
         detect_prec_list.append(detect_prec)
@@ -51,7 +55,7 @@ def main(epochs,  model_dir ='', train_flag=False, device_id=0, base_type='', de
     print('Old Model Acc before shift: {}%'.format(np.round(np.mean(acc_list),decimals=3)))
     print('Old Model Acc after shift: {}%'.format(np.round(np.mean(acc_shift_list),decimals=3)))
     print('Shifted Data Proportion on Old Model Misclassifications: {}%'.format(np.round(np.mean(np.array(shift_list),axis=0), decimals=3)))
-    Detector.statistics(detect_prec_list, 'precision')
+    Detector.statistics(detect_prec_list)
 
     split_data = ds.dataset
     for spit_name in split_data.keys():
