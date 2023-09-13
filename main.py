181f3f98b99590cb7f04a7c29351669e49058f7b
yiwei <e-well@outlook.com>
2023-09-12 22:28:34 -0400

    feat: detector acc during acquisition

--- main.py
+++ main.py
@@ -12,9 +12,23 @@ def data_run(new_img_num_list, operation: Config.Operation, new_model_config:Con
         run(operation, new_model_config, workspace)
 
 def method_run(method, new_img_num_list, new_model_config:Config.NewModel, operation: Config.Operation, workspace: WorkSpace):
+
+    if method != 'rs':
+        workspace.set_detector(operation.detection)
+        workspace.set_validation(operation.stream, new_model_config.batch_size, new_model_config.new_batch_size)
+        logger.info('set validation_loader') # Align with inference on the test set
+        _, detect_acc= workspace.detector.predict(workspace.data_split.loader['test_shift'], workspace.base_model, True)
+
+    else:
+        workspace.validation_loader = workspace.data_split.loader['val_shift']
+        logger.info('Keep val_shift for validation_loader') # Align with inference on the test set
+        detect_acc = 0
+
     operation.acquisition.method = method
     operation.acquisition = Config.AcquisitionFactory(operation.acquisition)
     data_run(new_img_num_list, operation, new_model_config, workspace)
+
+    return detect_acc
 
 def epoch_run(parse_args, method, n_data_list, dataset:dict, epo, operation: Config.Operation):
 
@@ -30,27 +44,27 @@ def epoch_run(parse_args, method, n_data_list, dataset:dict, epo, operation: Con
     
     workspace.set_up(new_model_config.new_batch_size, operation.detection.vit)
 
-    if method != 'rs':
-        workspace.set_detector(operation.detection)
-        workspace.set_validation(operation.stream, new_model_config.batch_size, new_model_config.new_batch_size)
-    else:
-        workspace.validation_loader = workspace.data_split.loader['val_shift']
-        logger.info('Keep val_shift')
-
     if filter_market:
         known_labels = config['data']['labels']['cover']['target']
         workspace.set_market(operation.detection.vit, known_labels)
-
-    method_run(method, n_data_list, new_model_config, operation, workspace)
+   
+    detect_acc = method_run(method, n_data_list, new_model_config, operation, workspace)
+    
+    return detect_acc
 
 def bound_run(parse_args, epochs, ds_list, method_list, bound, n_new_data_list, operation: Config.Operation):
 
     operation.acquisition.bound = bound
+    detect_acc_list = []
 
     for epo in range(epochs):
         logger.info('In epoch {}'.format(epo))
         dataset = ds_list[epo]
-        epoch_run(parse_args, method_list, n_new_data_list, dataset, epo, operation)
+        detect_acc = epoch_run(parse_args, method_list, n_new_data_list, dataset, epo, operation)
+        detect_acc_list.append(detect_acc)
+
+    logger.info('Detect Acc: {}'.format(np.mean(detect_acc_list)))
+    logger.info('All: {}'.format(detect_acc_list))
 
 def main(epochs, dev_name, device, detector_name, model_dir, base_type, option, dataset_name, filter_market=False):
 
