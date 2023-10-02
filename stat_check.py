f48c056d9ebacb28b65104aeba498fe883417078
yiwei <e-well@outlook.com>
2023-10-01 23:17:04 -0400

    feat: split stat check for test & train data

--- stat_check.py
+++ stat_check.py
@@ -5,32 +5,28 @@ import utils.statistics.distribution as Distribution
 from typing import List
 from utils.logging import *
 
-def get_errors(model: Model.Prototype, dataloader):
-    '''
-    Model Errors on a dataloader
-    '''
-    dataset_gts, dataset_preds, _ = model.eval(dataloader)
-    error_mask = (dataset_gts != dataset_preds)
-    return dataset_preds[error_mask]
-
 class TestData():
     def __init__(self) -> None:
         pass
    
     def error_stat(self, checker:Checker.Probability):
         if len(checker.test_loader['new_model']) == 0:
-            result = 0
+            new_error_stat = 0
             logger.info('No data in New Model Test')
         else:
-            test_data_size = dataloader_utils.get_size(checker.test_loader['new_model'])
-            error = get_errors(checker.base_model, checker.test_loader['new_model'])
-            result = len(error) / test_data_size  * 100
-            logger.info('Hard images in New Model Test: {}%'.format(result))
-            logger.info('New model test set size:{}'.format(test_data_size))
-        return result
+            new_error_stat = 100 - checker.base_model.acc(checker.test_loader['new_model'])
+            
+        if len(checker.test_loader['old_model']) == 0:
+            old_error_stat = 0
+            logger.info('No data in Old Model Test')
+        else:
+            old_error_stat = 100 - checker.base_model.acc(checker.test_loader['old_model'])
+
+        return [new_error_stat, old_error_stat]
     
-    def epoch_run(self, operation:Config.Operation, new_img_num_list, checker:Checker.Probability):
+    def seq_run(self, operation:Config.Operation, new_img_num_list, checker:Checker.Probability):
         operation.acquisition.method = 'seq'
+        seq_error_stat = []
         for n_img in new_img_num_list:
             operation.acquisition.n_ndata = n_img
             checker.new_model_config.set_path(operation)
@@ -39,16 +35,21 @@ class TestData():
             checker.detector = detector_log.import_log(operation, checker.general_config)
             anchor_dstr = checker.set_up_dstr(checker.anchor_loader, operation.stream.pdf)
             checker.test_loader, _ = checker.get_subset_loader(anchor_dstr, operation.stream)
-            _ = self.error_stat(checker)
+            seq_error_stat.append(self.error_stat(checker)[0]) # only fetch error in mew model test set
+        return seq_error_stat
         
     def run(self, epochs, parse_args, new_img_num_list, method, operation:Config.Operation, dataset_list: List[dict], plot):
         results = []
         for epo in range(epochs):
             logger.info('in epoch {}'.format(epo))
             checker = Checker.instantiate(epo, parse_args, dataset_list[epo], operation)
-            results.append(self.error_stat(checker))
-            if method == 'seq':
-                self.epoch_run(operation, new_img_num_list, checker)   # update checker's detector by logs
+            datasplits = dataset_utils.DataSplits(dataset_list[epo], checker.new_model_config.new_batch_size)
+            if 'seq' in method:
+                seq_error_stat = self.seq_run(operation, new_img_num_list, checker)   # update checker's detector by logs
+                results.append(seq_error_stat)
+            else:
+                results.append(self.error_stat(checker))
+
         if plot:
             self.plot(checker, dataset_list[epo], operation.stream.pdf)
 
@@ -56,28 +57,27 @@ class TestData():
     
     def plot(self, checker: Checker.Probability, dataset, pdf):
         datasplits = dataset_utils.DataSplits(dataset, checker.new_model_config.new_batch_size)
-        fig_name = 'figure/test/val_dv.png'
-        self.naive_plot(datasplits.loader['val_shift'], fig_name, checker.detector)
-        fig_name = 'figure/test/market_dv.png'
-        self.naive_plot(datasplits.loader['market'], fig_name, checker.detector)
+        # fig_name = 'figure/test/val_dv.png'
+        # self.naive_plot(datasplits.loader['val_shift'], fig_name, checker.detector)
+        # fig_name = 'figure/test/market_dv.png'
+        # self.naive_plot(datasplits.loader['market'], fig_name, checker.detector)
         fig_name = 'figure/test/anchor_dv.png'
         incor_dv = DataStat.get_correctness_dv(checker.base_model, datasplits.loader['val_shift'], checker.detector, correctness=False)
         cor_dv = DataStat.get_correctness_dv(checker.base_model, datasplits.loader['val_shift'], checker.detector, correctness=True)
         logger.info('Incorrection DSTR - max: {}, min:{}'.format(max(incor_dv), min(incor_dv)))
         logger.info('Correction DSTR - max: {}, min:{}'.format(max(cor_dv), min(cor_dv)))
-
         self.correctness_dstr_plot(cor_dv, incor_dv, fig_name, pdf)
-        fig_name = 'figure/test/test_dv.png'
-        incor_dv = DataStat.get_correctness_dv(checker.base_model, datasplits.loader['test_shift'], checker.detector, correctness=False)
-        cor_dv = DataStat.get_correctness_dv(checker.base_model, datasplits.loader['test_shift'], checker.detector, correctness=True)
-        self.correctness_dstr_plot(cor_dv, incor_dv, fig_name, pdf)
+        # fig_name = 'figure/test/test_dv.png'
+        # incor_dv = DataStat.get_correctness_dv(checker.base_model, datasplits.loader['test_shift'], checker.detector, correctness=False)
+        # cor_dv = DataStat.get_correctness_dv(checker.base_model, datasplits.loader['test_shift'], checker.detector, correctness=True)
+        # self.correctness_dstr_plot(cor_dv, incor_dv, fig_name, pdf)
 
     def correctness_dstr_plot(self, cor_dv, incor_dv, fig_name, pdf_method=None):
-        distribution_utils.base_plot(cor_dv, 'non-error', 'orange', pdf_method)
-        distribution_utils.base_plot(incor_dv, 'error', 'blue', pdf_method)
-        distribution_utils.plt.xlabel('Feature Score')
-        distribution_utils.plt.ylabel('Density')
-        distribution_utils.plt.title('Old Model Performance Feature Score Distribution')
+        distribution_utils.base_plot(-cor_dv, 'Easy Data', 'white', pdf_method, hatch_style='/')
+        distribution_utils.base_plot(-incor_dv, 'Hard Data', 'white', pdf_method, hatch_style='.', line_style=':', alpha=0.5)
+        distribution_utils.plt.xlabel('Error Feature Score', fontsize='large')
+        distribution_utils.plt.ylabel('Density', fontsize='large')
+        # distribution_utils.plt.title('Model Performance Feature Score Distribution')
         distribution_utils.plt.savefig(fig_name)
         distribution_utils.plt.close()
         logger.info('Save fig to {}'.format(fig_name))
@@ -94,9 +94,14 @@ class TestData():
         logger.info('Save fig to {}'.format(fig_name))
 
 class TrainData():
-    def __init__(self) -> None:
-        pass
-
+    def __init__(self, dataset_name, data_config, normalize_stat) -> None:
+        if dataset_name == 'cifar':
+            self.data = dataset_utils.Cifar().get_raw_dataset(data_config['root'], normalize_stat, data_config['labels']['map'])['train_market']
+        else:
+            meta_path = os.path.join('rate_data/meta/s2.pkl')
+            sampled_meta = dataset_utils.MetaData(meta_path)
+            self.data = dataset_utils.Core().get_raw_dataset(sampled_meta, normalize_stat, data_config['labels']['map'])
+        
     def dv_dstr_plot(self, cor_dv, incor_dv, n_data, pdf_method=None, range=None):
         pdf_name = '' if pdf_method == None else '_{}'.format(pdf_method)
         Distribution.base_plot(cor_dv, 'correct', 'orange', pdf_method, range)
@@ -127,24 +132,14 @@ class TrainData():
             acc_change_list.append(acc_change)
         return acc_change_list
 
-    def check_data(self, operation:Config.Operation, data_split:dataset_utils.DataSplits, checker:Checker.Prototype):
-        model_config = checker.new_model_config
-        model_config.set_path(operation)
-        log = Log(model_config, 'data')
-        new_data = log.import_log(operation, checker.general_config)
-        new_data_loader = torch.utils.data.DataLoader(new_data, batch_size= model_config.new_batch_size)
-        new_data_size = dataloader_utils.get_size(new_data_loader)
-        errors = get_errors(checker.base_model, new_data_loader)
-        return len(errors) / new_data_size * 100
-
     def check_indices(self, operation:Config.Operation, data_split:dataset_utils.DataSplits, checker:Checker.Prototype, pdf_method, distribution: distribution_utils.Disrtibution):
         model_config = checker.new_model_config
         model_config.set_path(operation)
 
         log = Log(model_config, 'indices')
         new_data_indices = log.import_log(operation, checker.general_config)
-        new_data = torch.utils.data.Subset(data_split.dataset['market'], new_data_indices)
-        new_data_loader = torch.utils.data.DataLoader(new_data, batch_size= model_config.new_batch_size)
+        new_data = torch.utils.data.Subset(self.data, new_data_indices)
+        new_data_loader = torch.utils.data.DataLoader(new_data, batch_size=model_config.new_batch_size)
 
         # new_data_dv, _ = checker.detector.predict(new_data_loader, checker.base_model)
 
@@ -152,13 +147,9 @@ class TrainData():
 
         # return probab
 
-        new_data_size = dataloader_utils.get_size(new_data_loader)
-        
         # old_labels = set(Subset.config['data']['train_label']) - set(Subset.config['data']['remove_fine_labels'])
         # logger.info(Subset.label_stat(new_data, Subset.config['data']['remove_fine_labels']), Subset.label_stat(new_data, old_labels))
-        errors = get_errors(checker.base_model, new_data_loader)
-
-        return len(errors) / new_data_size * 100
+        return 100 - checker.base_model.acc(new_data_loader)
     
         # train_loader = torch.utils.data.DataLoader(data_split.dataset['train'], batch_size= model_config.new_batch_size)
         # base_gt, base_pred, _ = checker.base_model.eval(train_loader)
@@ -188,16 +179,17 @@ class TrainData():
 
     def n_data_run(self, operation:Config.Operation, checker: Checker.Prototype, data_split:dataset_utils.DataSplits, pdf_method):
         if 'seq' in operation.acquisition.method:
-            # check_result = self.check_data(operation, data_split, checker)
+            # check_result = self.check_indices(operation, data_split, checker, pdf_method, None)
             check_result = self.check_clf(operation, data_split, checker)
         else:
             distribution = distribution_utils.Disrtibution(checker.detector, data_split.loader['val_shift'], operation.stream.pdf)
             check_result = self.check_indices(operation, data_split, checker, pdf_method, distribution)
         return check_result
    
+
 def main(epochs, new_model_setter='retrain', model_dir ='', device=0, probab_bound = 0.5, base_type='', detector_name = '', opion = '', dataset_name = '', stat_data='train', dev_name= 'dv'):
     pure = True
-    fh = logging.FileHandler('log/{}/stat_{}_{}.log'.format(model_dir, dev_name, stat_data),mode='w')
+    fh = logging.FileHandler('rate_log/{}/stat_{}_{}.log'.format(model_dir, dev_name, stat_data), mode='w')
     fh.setLevel(logging.INFO)
     logger.addHandler(fh)
     logger.info('Detector: {}; Probab bound: {}'.format(detector_name, probab_bound))
@@ -216,7 +208,7 @@ def main(epochs, new_model_setter='retrain', model_dir ='', device=0, probab_bou
     parse_args = (model_dir, device_config, base_type, pure, new_model_setter, config)
     
     if stat_data == 'train':
-        stat_checker = TrainData()
+        stat_checker = TrainData(dataset_name, config['data'], normalize_stat)
         results = stat_checker.run(epochs, parse_args, config['data']['n_new_data'], method, operation, ds_list, stream_instruction.pdf)
         results = np.array(results)
         logger.info(method)
@@ -224,8 +216,8 @@ def main(epochs, new_model_setter='retrain', model_dir ='', device=0, probab_bou
         logger.info('all: {}'.format(results.tolist()))
     else:
         stat_checker = TestData()
-        results = stat_checker.run(epochs, parse_args, config['data']['n_new_data'], method, operation, ds_list, plot=True)
-        logger.info('Test Data error stat:{}'.format(np.round(np.mean(results), decimals=3)))
+        results = stat_checker.run(epochs, parse_args, config['data']['n_new_data'], method, operation, ds_list, plot=False)
+        logger.info('Test Data error stat:{}'.format(np.round(np.mean(results, axis=0), decimals=3)))
         logger.info('all: {}'.format(results))
 
 import argparse
