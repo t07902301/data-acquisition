ec4c34dcad0ad97673d397b7da21d679456ccc76
yiwei <e-well@outlook.com>
2023-12-30 11:38:12 -0500

    ue when it's not random sampling, weakness labels

--- main.py
+++ main.py
@@ -7,21 +7,15 @@ def run(operation: Config.Operation, new_model_config:Config.NewModel, workspace
     strategy.operate(operation, new_model_config, workspace)
 
 def budget_run(budget_list, operation: Config.Operation, new_model_config:Config.NewModel, workspace: WorkSpace):
-    workspace.set_utility_estimator(operation.detection, operation.acquisition.utility_estimation, operation.ensemble.pdf)
     for budget in budget_list:
         operation.acquisition.set_budget(budget)
         run(operation, new_model_config, workspace)
 
 def method_run(budget_list, new_model_config:Config.NewModel, operation: Config.Operation, workspace: WorkSpace):
     
-    # method = operation.acquisition.method
-    # if method != 'rs':
-    #     workspace.set_utility_estimator(operation.detection, operation.acquisition.utility_estimation, operation.ensemble.pdf)
-    #     workspace.set_validation(new_model_config.new_batch_size)
-    # else:
-    #     workspace.validation_loader = workspace.data_split.loader['val_shift']
-    #     logger.info('Keep val_shift for validation_loader') # Align with inference on the test set
-
+    if operation.acquisition.method != 'rs': # to be integrated to utility estimator as random number generator
+        workspace.set_utility_estimator(operation.detection, operation.acquisition.utility_estimation, operation.ensemble.pdf)
+    
     budget_run(budget_list, operation, new_model_config, workspace)
 
 def epoch_run(parse_args:ParseArgs, budget_list, dataset:dict, epo, operation: Config.Operation):
@@ -40,7 +34,7 @@ def epoch_run(parse_args:ParseArgs, budget_list, dataset:dict, epo, operation: C
    
     method_run(budget_list, new_model_config, operation, workspace)
 
-def main(epochs, acquisition_method, device, detector_name, model_dir, utility_estimator, filter_market=False):
+def main(epochs, acquisition_method, device, detector_name, model_dir, utility_estimator, weaness_label_generator, filter_market=False):
 
     fh = logging.FileHandler('log/{}/{}.log'.format(model_dir, acquisition_method),mode='w')
     fh.setLevel(logging.DEBUG)
@@ -53,7 +47,7 @@ def main(epochs, acquisition_method, device, detector_name, model_dir, utility_e
     
     clip_processor = Detector.load_clip(parse_args.device_config, normalize_stat['mean'], normalize_stat['std'])
     ensemble_instruction = Config.Ensemble()
-    detect_instruction = Config.Detection(detector_name, clip_processor)
+    detect_instruction = Config.Detection(detector_name, clip_processor, weaness_label_generator)
     acquire_instruction = Config.AcquisitionFactory(acquisition_method=acquisition_method, data_config=parse_args.general_config['data'], utility_estimator=utility_estimator)
 
     operation = Config.Operation(acquire_instruction, ensemble_instruction, detect_instruction)
@@ -75,6 +69,7 @@ if __name__ == '__main__':
     parser.add_argument('-ue','--utility_estimator',type=str, default='u-ws', help="u-ws, u-wsd")
 
     parser.add_argument('-am','--acquisition_method',type=str, default='dv', help="Acquisition Strategy; dv:one-shot, rs: random, conf: Probability-at-Ground-Truth, mix: Random Weakness, seq: sequential, pd: one-shot + u-wsd, seq_pd: seq + u-wsd")
+    parser.add_argument('-wc','--weaness_label_generator',type=str,default='correctness', help="correctness, entropy+gmm")
 
     args = parser.parse_args()
-    main(args.epochs, model_dir=args.model_dir, device=args.device, detector_name=args.detector_name, acquisition_method=args.acquisition_method, utility_estimator=args.utility_estimator)
+    main(args.epochs, model_dir=args.model_dir, device=args.device, detector_name=args.detector_name, acquisition_method=args.acquisition_method, utility_estimator=args.utility_estimator, weaness_label_generator=args.weaness_label_generator)
